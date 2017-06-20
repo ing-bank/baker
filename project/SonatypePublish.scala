1191dbd65cf3c3a416607aa94336e58ca613a20a
Bekir Oguz <bekiroguz@gmail.com>
2017-06-20 12:20:45 +0200

    defined the sonatype profile name

--- project/SonatypePublish.scala
+++ project/SonatypePublish.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys._
 import sbtrelease.ReleasePlugin.autoImport._
 import sbtrelease.ReleaseStateTransformations._
+import xerial.sbt.Sonatype.SonatypeKeys._
 
 object SonatypePublish {
 
@@ -12,21 +13,22 @@ object SonatypePublish {
   protected val ossStaging = "Sonatype OSS Staging" at nexus + "service/local/staging/deploy/maven2/"
 
   val settings = Seq(
+    sonatypeProfileName := "com.ing",
     licenses := Seq("MIT" -> url("https://opensource.org/licenses/MIT")),
     homepage := Some(url("https://github.com/ing-bank/baker")),
     pomExtra := (
-        <scm>
+      <scm>
           <url>https://github.com/ing-bank/baker</url>
           <connection>scm:git:https://github.com/ing-bank/baker.git</connection>
           <developerConnection>scm:git:git@github.com:ing-bank/baker.git</developerConnection>
         </scm>
-        <developers>
+  <developers>
           <developer>
             <id>Apollo</id>
             <name>Squad Apollo</name>
           </developer>
         </developers>
-      ),
+  ),
     publishMavenStyle := true,
     publishTo <<= version((v: String) => Some(if (isSnapshot(v)) ossSnapshots else ossStaging)),
     publishArtifact in Test := false,
