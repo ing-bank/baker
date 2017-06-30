96acacec7c968b944d18e5eed9c18bea32ec711b
Merlijn van Ittersum <merlijn.van.ittersum@ing.com>
2017-06-30 10:24:24 +0200

    removed some reflection code from intermediate language module

--- intermediate-language/src/main/scala/com/ing/baker/il/EventType.scala
+++ intermediate-language/src/main/scala/com/ing/baker/il/EventType.scala
@@ -1,10 +1,3 @@
 package com.ing.baker.il
 
-case class EventType(name: String,
-                     providedIngredients: Seq[IngredientType])
-
-object EventType {
-  def apply(obj: Any): EventType = {
-    EventType(obj.getClass.getSimpleName, obj.getClass.getDeclaredFields.map(IngredientType(_)))
-  }
-}
+case class EventType(name: String, providedIngredients: Seq[IngredientType])
