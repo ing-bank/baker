1bd6d8dcf5cef1a648bb0c82109c80455587c211
Bekir Oguz <bekiroguz@gmail.com>
2017-07-11 17:29:01 +0200

    output the recipe visualization to a predefined output dir.

--- test-module/src/test/scala/com/ing/baker/pbt/RecipeProperties.scala
+++ test-module/src/test/scala/com/ing/baker/pbt/RecipeProperties.scala
@@ -1,6 +1,10 @@
 package com.ing.baker.pbt
 
+import java.io.{File, PrintWriter}
+import java.util.UUID
+
 import com.ing.baker.compiler.RecipeCompiler
+import com.ing.baker.il.CompiledRecipe
 import com.ing.baker.recipe.common.FiresOneOfEvents
 import com.ing.baker.recipe.scaladsl.{Event, Ingredient, Interaction, InteractionDescriptor, Recipe}
 import org.scalacheck.Prop.forAll
@@ -9,17 +13,19 @@ import org.scalacheck._
 import scala.util.Random
 
 class RecipeProperties extends Properties("Properties of a Recipe") {
+
   import RecipeProperties._
 
   property("recipe compiles without validation errors") = forAll(recipeGen) { recipe =>
 
-//    println(s"===============================================================Recipe under test: ${recipe.name}")
-//    println(s"Interactions: ${recipe.interactions.map(_.name).mkString(" ")}")
-//    println(s"Interaction Outputs: ${recipe.interactions.map(_.interaction.output).mkString(" ")}")
-//    println(s"Sensory events: ${recipe.sensoryEvents.map(_.name).mkString(" ")}")
-//    println("===============================================================")
+    //    println(s"===============================================================Recipe under test: ${recipe.name}")
+    //    println(s"Interactions: ${recipe.interactions.map(_.name).mkString(" ")}")
+    //    println(s"Interaction Outputs: ${recipe.interactions.map(_.interaction.output).mkString(" ")}")
+    //    println(s"Sensory events: ${recipe.sensoryEvents.map(_.name).mkString(" ")}")
+    //    println("===============================================================")
 
     val compiledRecipe = RecipeCompiler.compileRecipe(recipe)
+
     println(s"PBT stats: recipeName: ${compiledRecipe.name} " +
       s"nrOfAllIngredients: ${compiledRecipe.ingredients.size} " +
       s"nrOfSensoryEvents: ${compiledRecipe.sensoryEvents.size} " +
@@ -31,9 +37,7 @@ class RecipeProperties extends Properties("Properties of a Recipe") {
       println(s"Visual recipe: ${compiledRecipe.getRecipeVisualization}")
     }
 
-//    if (compiledRecipe.interactionTransitions.size > 2) {
-//      println(s"Visual recipe: ${compiledRecipe.getRecipeVisualization}")
-//    }
+    dumpVisualRecipe(recipeVisualizationOutputPath, compiledRecipe)
 
     compiledRecipe.validationErrors.isEmpty
   }
@@ -41,80 +45,95 @@ class RecipeProperties extends Properties("Properties of a Recipe") {
 }
 
 object RecipeProperties {
-  val sampleSize = 10
-  val ingredientPoolForSensoryEvents: Seq[Ingredient[_]] = for (idx <- 0 until sampleSize) yield Ingredient[String](s"ingredient-$idx")
+  ////////////////// start configuration
+
+  // These parameters determine the size of the recipes
+  val initialIngredientPoolUsedBySensoryEvents = 100
+  val maxNrOfIngredientsPerEvent = 5
+  val maxNrOfEventsComingInToAnInteraction = 5
+  val maxNrOfEventsGoingOutFromAnInteraction = 5
+
+  // where to output the .dot files of the generated recipe visualizations
+  val recipeVisualizationOutputPath: String = System.getProperty("java.io.tmpdir")
+
+  ////////////////// end configuration
+
+  val ingredientPoolForSensoryEvents: Seq[Ingredient[_]] = for (idx <- 0 until initialIngredientPoolUsedBySensoryEvents) yield Ingredient[String](s"ingredient-$idx")
+
+  def rand(max: Int): Int = Random.nextInt(max)
+
+  def uuid(): String = UUID.randomUUID().toString
+
+  def randomIngredients(max: Int): Seq[Ingredient[_]] = for (_ <- 0 until rand(max)) yield Ingredient[String](s"ingredient-${uuid()}")
+
+  def randomEvents(max: Int): Seq[Event] = for (_ <- 0 until rand(max)) yield Event(s"event-${uuid()}", randomIngredients(max))
 
   val sensoryEventPool: Seq[Event] = {
     def pickEventLoop(idx: Int, ingredients: Seq[Ingredient[_]]): Seq[Event] = {
       if (ingredients.isEmpty) {
         val event = Event(s"event-$idx", ingredients)
-//        println(s"Generated event ${event.name} with ${ingredients.size} ingredients")
         Seq(event)
       } else {
-        val taken = Gen.someOf(ingredients).sample.getOrElse(Seq.empty)
+        val taken = ingredients.take(rand(maxNrOfIngredientsPerEvent))
         val remaining = ingredients.diff(taken)
 
         val event = Event(s"sensory-event-$idx", taken)
-//        println(s"Generated event ${event.name} with ${taken.size} ingredients")
-        Seq(event) ++ pickEventLoop(idx+1, remaining)
+        Seq(event) ++ pickEventLoop(idx + 1, remaining)
       }
     }
-//    println("***Generating sequence of events...")
+
     pickEventLoop(0, ingredientPoolForSensoryEvents)
   }
 
-  val ingredientGen: Gen[Ingredient[_]] = for {
-    ingredientName <- Gen.uuid.map(_.toString)
-  } yield Ingredient[String](s"ingredient-$ingredientName")
+  println(s"============ Started with $initialIngredientPoolUsedBySensoryEvents ingredients and generated ${sensoryEventPool.size} sensory events")
 
-  val interactionEventGen: Gen[Event] = for {
-    eventName <- Gen.uuid.map(_.toString)
-    ingredients <- Gen.listOfN(Random.nextInt(10), ingredientGen)
-  } yield Event(s"event-$eventName", ingredients)
-
-  println(s"============ EVENT POOL SIZE: ${sensoryEventPool.size}")
-
-  def interactions(events: Seq[Event], recipeName: String): Seq[InteractionDescriptor] = {
+  def interactions(events: Seq[Event]): Seq[InteractionDescriptor] = {
 
     def pickInteractionLoop(idx: Int, events: Seq[Event], interactionEvents: Seq[Event]): Seq[InteractionDescriptor] = {
-//      if (events.isEmpty || idx > 5) {
       if (events.isEmpty) {
         Seq.empty
       } else {
-        val takenEvents = Gen.someOf(events ++ interactionEvents).sample.getOrElse(events.take(1))
+        val takenEvents = (events ++ interactionEvents).take(rand(maxNrOfEventsComingInToAnInteraction))
         val inputIngredients = takenEvents.flatMap(_.providedIngredients)
         val remainingEvents = events.diff(takenEvents)
 
-        val takenInteractionEvents = Gen.listOfN(Random.nextInt(10), interactionEventGen).sample.getOrElse(Seq.empty)
-
         // Do not generate an interaction with 0 ingredients, this is not a valid recipe. Skip this round.
         if (inputIngredients.isEmpty) {
-//          println("Sample input ingredient list is empty, skipping this round of Interaction generation.")
           Seq.empty ++ pickInteractionLoop(idx, remainingEvents, interactionEvents)
         } else {
-          val interactionOutput = FiresOneOfEvents(takenInteractionEvents)
+          val interactionEvents = randomEvents(maxNrOfEventsGoingOutFromAnInteraction)
+          val interactionOutput = FiresOneOfEvents(interactionEvents)
           val interaction = InteractionDescriptor(Interaction(s"interaction-$idx", inputIngredients, interactionOutput))
-//          println(s"Generated interaction ${interaction.name} with ${inputIngredients.size} input ingredients")
-          Seq(interaction) ++ pickInteractionLoop(idx+1, remainingEvents, interactionEvents ++ takenInteractionEvents)
+          Seq(interaction) ++ pickInteractionLoop(idx + 1, remainingEvents, interactionEvents ++ interactionEvents)
         }
 
       }
     }
 
-//    println(s"***Generating sequence of interactions for recipe $recipeName... with initial events: $events")
     pickInteractionLoop(0, events, Seq.empty)
   }
 
   val recipeGen: Gen[Recipe] = for {
-    recipeName <- Gen.uuid.map(_.toString)
     sensoryEvents <- Gen.someOf(sensoryEventPool)
-//    interactionEvents <- Gen.someOf(eventPool.diff(sensoryEvents))
-//    _ = println(s"nrOfSensoryEvents=${sensoryEvents.size}")
-  //    _ = sensoryEvents.foreach(event => println(s"ingredients in each event: ${event.providedIngredients.size}"))
-  } yield Recipe(s"recipe-$recipeName")
+  } yield Recipe(s"recipe-${uuid()}")
     .withSensoryEvents(sensoryEvents: _*)
-    .withInteractions(interactions(sensoryEvents, recipeName): _*)
+    .withInteractions(interactions(sensoryEvents): _*)
 
+  def dumpVisualRecipe(dumpDir: String, compiledRecipe: CompiledRecipe): Unit = {
+    val fileName =
+      if (dumpDir endsWith "/") s"$dumpDir${compiledRecipe.name}.dot"
+      else s"$dumpDir/${compiledRecipe.name}.dot"
+
+    val writer = new PrintWriter(new File(fileName))
+
+    try {
+      println(s"Dumping the visual recipe here: $fileName")
+      writer.write(compiledRecipe.getRecipeVisualization)
+    } finally {
+      writer.close()
+    }
+
+  }
 
 }
 
