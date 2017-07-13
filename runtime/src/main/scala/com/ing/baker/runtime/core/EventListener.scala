9fb99021f0da357810ca03f729020e2bd116b48e
Merlijn van Ittersum <merlijn.van.ittersum@ing.com>
2017-07-13 11:29:49 +0200

    documentation

--- runtime/src/main/scala/com/ing/baker/runtime/core/EventListener.scala
+++ runtime/src/main/scala/com/ing/baker/runtime/core/EventListener.scala
@@ -1,6 +1,15 @@
 package com.ing.baker.runtime.core
 
+/**
+  * Listener interface for events from baker.
+  */
 trait EventListener {
 
+  /**
+    * Called when an event occured.
+    *
+    * @param processId The process id for which the event occured.
+    * @param event The event.
+    */
   def processEvent(processId: String, event: RuntimeEvent)
 }
