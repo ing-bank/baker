a4b6c78971ae95c3b5f5fa69e50fb86da0d8cc0e
Tim Linschoten <timlinschoten@outlook.com>
2017-08-03 14:55:14 +0200

    Max time between retries (no jitter) (#25)
    * Refactored the failureStrategy in the baker il to make more sense.
    Added a optional maxRetryTime to the RetryWithIncrementalBackoff to the IL.
    Added unit tests for the RetryWithIncrementalBackoff.
    
    * Update the dsl to allow the setting of the MaxTimeBetweenRetries.
    
    * Refactored the dsl on how the FailureStrategy is set.
    Now the FailureStrategy is given to the dsl instead of a function on the recipe and Interaction per type of failure strategy.
    This allows for adding and changing the failure strategy whiteout chaning other parts of the dsl.
    
    * Fixes go docs
    
    * Fix to BlockInteraction in the javadsl that had a reference to itself.
    This made building a recipe impossible.
    
    * Fixed an issue that occurred when using the maxTimeBetweenRetries.
    The times that where calculated where not done with the maxTimeBetweenRetries in mind.
    Therefor there where to little retries planned.
    
    * Fixed the failing test and removed unused code from the javadsl ReflectionHelpers.
    
    * Removed already done TODOS
    
    * small refactorings

--- recipe-dsl/src/main/scala/com/ing/baker/recipe/common/InteractionFailureStrategy.scala
+++ recipe-dsl/src/main/scala/com/ing/baker/recipe/common/InteractionFailureStrategy.scala
@@ -1,30 +1,60 @@
 package com.ing.baker.recipe.common
 
-import scala.concurrent.duration.{Duration, FiniteDuration}
+import scala.annotation.tailrec
+import scala.concurrent.duration.Duration
+
+sealed trait InteractionFailureStrategy
 
 object InteractionFailureStrategy {
 
+  case class BlockInteraction() extends InteractionFailureStrategy
+
   case class RetryWithIncrementalBackoff(initialTimeout: Duration,
-                                         backoffFactor: Double = 2.0,
-                                         maximumRetries: Int = 50)
-      extends InteractionFailureStrategy {
+                                         backoffFactor: Double,
+                                         maximumRetries: Int,
+                                         maxTimeBetweenRetries: Option[Duration] = None) extends InteractionFailureStrategy {
     require(backoffFactor >= 1.0, "backoff factor must be greater or equal to 1.0")
     require(maximumRetries >= 1, "maximum retries must be greater or equal to 1")
   }
 
   object RetryWithIncrementalBackoff {
-    def apply(initialDelay: FiniteDuration,
-              deadline: FiniteDuration): RetryWithIncrementalBackoff = {
+    def apply(initialDelay: Duration,
+              deadline: Duration,
+              maxTimeBetweenRetries: Option[Duration]): RetryWithIncrementalBackoff = {
+
       require(deadline > initialDelay, "deadline should be greater then initialDelay")
 
-      val backoffFactor: Double = 2.0
-      val maximumRetries =
-        (Math.log(deadline / initialDelay) / Math.log(backoffFactor)).round.toInt
+      val backoffFactor = 2.0
 
-      RetryWithIncrementalBackoff(initialDelay, backoffFactor, maximumRetries)
+      @tailrec def calculateMaxRetries(lastDelay: Duration,
+                                       backoffFactor: Double,
+                                       deadline: Duration,
+                                       totalDelay: Duration,
+                                       timesCounter: Int): Int = {
+
+        def min(d1Option: Option[Duration], d2: Duration): Duration = d1Option match {
+          case Some(d) if d < d2 => d
+          case _ => d2
+        }
+
+        val nextDelay = min(maxTimeBetweenRetries, lastDelay * backoffFactor)
+
+        if ((totalDelay + nextDelay) > deadline) timesCounter
+        else calculateMaxRetries(nextDelay, backoffFactor, deadline, totalDelay + nextDelay, timesCounter + 1)
+      }
+
+      new RetryWithIncrementalBackoff(
+        initialTimeout = initialDelay,
+        backoffFactor,
+        maximumRetries = calculateMaxRetries(
+          lastDelay = initialDelay,
+          backoffFactor,
+          deadline,
+          totalDelay = initialDelay,
+          timesCounter = 1),
+        maxTimeBetweenRetries)
     }
   }
-  case object BlockInteraction extends InteractionFailureStrategy
+
 }
 
-sealed trait InteractionFailureStrategy
