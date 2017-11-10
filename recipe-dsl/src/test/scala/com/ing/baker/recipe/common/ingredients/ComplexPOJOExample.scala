9c7336ec8c7d1590505fa592865415a747efd42d
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2017-11-10 10:44:35 +0100

    removed unused test classes

--- recipe-dsl/src/test/scala/com/ing/baker/recipe/common/ingredients/ComplexPOJOExample.scala
+++
@@ -1,9 +0,0 @@
-package com.ing.baker.recipe.common.ingredients
-
-/**
-  * Created by XK00LJ on 31-10-2017.
-  */
-case class ComplexPOJOExample(simplePOJOExample: SimplePOJOExample,
-                              string: String,
-                              boolean: Boolean) {
-}
