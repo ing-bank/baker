eff7f83ad3a05402412edce403aaa8a255ce524f
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2018-03-01 16:59:28 +0100

    added java getters on ProcessState

--- runtime/src/main/scala/com/ing/baker/runtime/core/ProcessState.scala
+++ runtime/src/main/scala/com/ing/baker/runtime/core/ProcessState.scala
@@ -2,4 +2,13 @@ package com.ing.baker.runtime.core
 
 import com.ing.baker.types.Value
 
-case class ProcessState(processId: String, ingredients: Map[String, Value], eventNames: List[String]) extends Serializable
+import scala.collection.JavaConverters._
+
+case class ProcessState(processId: String, ingredients: Map[String, Value], eventNames: List[String]) extends Serializable {
+
+  def getIngredients(): java.util.Map[String, Value] = ingredients.asJava
+
+  def getEventNames(): java.util.List[String] = eventNames.asJava
+
+  def getProcessId(): String = processId
+}
