c05ed2524a6be92591646451631a80b507d2832f
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2018-03-27 14:12:20 +0200

    improve javadoc of ProcessMetadata class

--- runtime/src/main/scala/com/ing/baker/runtime/core/ProcessMetadata.scala
+++ runtime/src/main/scala/com/ing/baker/runtime/core/ProcessMetadata.scala
@@ -1,17 +1,35 @@
 package com.ing.baker.runtime.core
 
 /**
-  * This class is to store some metadata information of a baker process
+  * This class holds some meta data of a baker process.
   *
-  * @param recipeId is dof the process
-  * @param processId id of the recipe
-  * @param createdTime process created date-time
+  * @param recipeId The recipe id of the process
+  * @param processId The identifier of the process
+  * @param createdTime The time the process was created
   */
 case class ProcessMetadata(recipeId: String, processId: String, createdTime: Long) {
 
+  /**
+    * Returns the recipe id of the process.
+    *
+    * @return The recipe id of the process.
+    */
   def getRecipeId(): String  = recipeId
 
+  /**
+    * Returns the process identifier.
+    *
+    * @return The process identifier.
+    */
   def getProcessId(): String = processId
 
+  /**
+    * Returns the time the process was created.
+    *
+    * The timestamp is measured as the difference, in milliseconds, between
+    * the process created time and midnight, January 1, 1970 UTC.
+    *
+    * @return The time the process was created.
+    */
   def getCreatedTime(): Long = createdTime
 }
