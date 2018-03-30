12457c2481a15470be364c5faf6402a1e77b40fa
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2018-03-30 16:32:05 +0200

    Added some javadoc

--- runtime/src/main/scala/com/ing/baker/runtime/core/ProcessState.scala
+++ runtime/src/main/scala/com/ing/baker/runtime/core/ProcessState.scala
@@ -4,6 +4,13 @@ import com.ing.baker.types.Value
 
 import scala.collection.JavaConverters._
 
+/**
+  * Describes the state of a process
+  *
+  * @param processId The process identifier
+  * @param ingredients The collected ingredients
+  * @param eventNames A list names of the event that occurred
+  */
 case class ProcessState(processId: String, ingredients: Map[String, Value], eventNames: List[String]) extends Serializable {
 
   def getIngredients(): java.util.Map[String, Value] = ingredients.asJava
--- runtime/src/main/scala/com/ing/baker/runtime/core/SensoryEventStatus.java
+++ runtime/src/main/scala/com/ing/baker/runtime/core/SensoryEventStatus.java
@@ -1,10 +1,41 @@
 package com.ing.baker.runtime.core;
 
+/**
+ *  Describes how baker processed an event.
+ */
 public enum SensoryEventStatus {
+    
+    /**
+     * The event was received.
+     */
     Received,
+
+    /**
+     * The event was received and all subsequent interactions processed.
+     */
     Completed,
+
+    /**
+     *  The event was not accepted because the firing limit
+     *  of that event was met.
+     */
     FiringLimitMet,
+
+    /**
+     * The event was not accepted because the receive period
+     * in which events can be received for the recipe was passed.
+     */
     ReceivePeriodExpired,
+
+    /**
+     * The event was not accepted because it was already
+     * received before.
+     */
     AlreadyReceived,
+
+    /**
+     * The event was not accepted because the process
+     * was deleted.
+     */
     ProcessDeleted
 }
