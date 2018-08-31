6c0423592bc2d2350417fa309af452940b358463
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2018-08-31 11:13:01 +0200

    increase the tested bounds for joda time instances

--- bakertypes/src/test/scala/com/ing/baker/types/modules/JodaTimeModuleSpec.scala
+++ bakertypes/src/test/scala/com/ing/baker/types/modules/JodaTimeModuleSpec.scala
@@ -12,7 +12,7 @@ import scala.reflect.runtime.universe.TypeTag
 
 class JodaTimeModuleSpec extends WordSpecLike with Matchers with Checkers {
 
-  def transitivityProp[T : TypeTag](gen: Gen[T]) = forAll(gen) { original =>
+  def transitivityProperty[T : TypeTag](gen: Gen[T]) = forAll(gen) { original =>
 
     // assertion of the result
     val value = Converters.toValue(original)
@@ -22,6 +22,15 @@ class JodaTimeModuleSpec extends WordSpecLike with Matchers with Checkers {
   }
 
   val minSuccessfulTests = 100
+
+  // Long.MaxValue is not supported by joda time for local dates, resulting in a integer overflow
+  // This shifts the long max value 1 bit to the right (divides by 2)
+  // This translates to the date: Fri Apr 24 17:36:27 CEST 146140482
+  val maxMillis = Long.MaxValue >> 1
+
+  val numGen: Gen[Long] = Gen.chooseNum[Long](
+    0L, maxMillis, 0, maxMillis
+  )
 
   "The JodaTimeModule" should {
 
@@ -34,23 +43,23 @@ class JodaTimeModuleSpec extends WordSpecLike with Matchers with Checkers {
 
     "be able to read/write all DateTime instances" in {
 
-      val dateTimeGen: Gen[DateTime] = Gen.posNum[Long].map(millis => new DateTime(millis))
+      val dateTimeGen: Gen[DateTime] = numGen.map(millis => new DateTime(millis))
 
-      check(transitivityProp[DateTime](dateTimeGen), defaultVerbose.withMinSuccessfulTests(minSuccessfulTests))
+      check(transitivityProperty[DateTime](dateTimeGen), defaultVerbose.withMinSuccessfulTests(minSuccessfulTests))
     }
 
     "be able to read/write all LocalDateTime instances" in {
 
-      val localDateTimeGen: Gen[LocalDateTime] = Gen.posNum[Long].map(millis => new LocalDateTime(millis))
+      val localDateTimeGen: Gen[LocalDateTime] = numGen.map(millis => new LocalDateTime(millis))
 
-      check(transitivityProp[LocalDateTime](localDateTimeGen), defaultVerbose.withMinSuccessfulTests(minSuccessfulTests))
+      check(transitivityProperty[LocalDateTime](localDateTimeGen), defaultVerbose.withMinSuccessfulTests(minSuccessfulTests))
     }
 
     "be able to read/write all LocalDate instances" in {
 
-      val localDateGen: Gen[LocalDate] = Gen.posNum[Long].map(millis => new LocalDate(millis))
+      val localDateGen: Gen[LocalDate] = numGen.map(millis => new LocalDate(millis))
 
-      check(transitivityProp[LocalDate](localDateGen), defaultVerbose.withMinSuccessfulTests(minSuccessfulTests))
+      check(transitivityProperty[LocalDate](localDateGen), defaultVerbose.withMinSuccessfulTests(minSuccessfulTests))
     }
   }
 }
