f34a6fd0fbd988867c379d56e896dab25e9fd434
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2018-09-18 09:50:16 +0200

    optimization in ProtoEventAdapter

--- runtime/src/main/scala/com/ing/baker/runtime/actor/serialization/ProtoEventAdapter.scala
+++ runtime/src/main/scala/com/ing/baker/runtime/actor/serialization/ProtoEventAdapter.scala
@@ -13,20 +13,28 @@ class ProtoEventAdapter(override val objectSerializer: ObjectSerializer) extends
     new TypesModule
   )
 
+  // Combine all partial functions into one partial function
+  val toProtoPF: PartialFunction[AnyRef, scalapb.GeneratedMessage] =
+    registeredModules
+      .map(_.toProto(this))
+      .reduce(_ orElse _)
+
+  // Combine all partial functions into one partial function
+  val toDomainPF: PartialFunction[scalapb.GeneratedMessage, AnyRef] =
+    registeredModules
+      .map(_.toDomain(this))
+      .reduce(_ orElse _)
+
   def toProtoType[T <: scalapb.GeneratedMessage](obj: AnyRef): T = toProto(obj).asInstanceOf[T]
 
   def toDomainType[T](serializedMessage: scalapb.GeneratedMessage): T = toDomain(serializedMessage).asInstanceOf[T]
 
-  def toProto(obj: AnyRef): scalapb.GeneratedMessage = {
-    registeredModules.map(_.toProto(this)) // extract the partial function to apply
-      .reduce[PartialFunction[AnyRef, scalapb.GeneratedMessage]] { case (f1, f2) => f1 orElse f2 } // Combine all partial functions into one partial function
-      .lift.apply(obj).getOrElse(throw new IllegalStateException(s"Cannot serialize object of type ${obj.getClass}"))
-  }
+  def toProto(obj: AnyRef): scalapb.GeneratedMessage =
+    toProtoPF.lift.apply(obj).getOrElse(
+      throw new IllegalStateException(s"Cannot serialize object of type ${obj.getClass}"))
 
-  def toDomain(serializedMessage: GeneratedMessage): AnyRef = {
-    registeredModules.map(_.toDomain(this)) // extract the partial function to apply
-      .reduce[PartialFunction[scalapb.GeneratedMessage,AnyRef]] { case (f1, f2) => f1 orElse f2 } // Combine all partial functions into one partial function
-      .lift.apply(serializedMessage).getOrElse(throw new IllegalStateException(s"Unknown protobuf message of type ${serializedMessage.getClass}"))
-  }
+  def toDomain(serializedMessage: GeneratedMessage): AnyRef =
+    toDomainPF.lift.apply(serializedMessage).getOrElse(
+      throw new IllegalStateException(s"Unknown protobuf message of type ${serializedMessage.getClass}"))
 
 }
