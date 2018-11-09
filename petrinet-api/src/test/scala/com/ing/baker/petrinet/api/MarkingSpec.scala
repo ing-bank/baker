dad50b8cefd17bf1cdd032a95f8cbeb884545160
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2018-11-09 10:57:52 +0100

    MarkingSpec does not need dsl

--- petrinet-api/src/test/scala/com/ing/baker/petrinet/api/MarkingSpec.scala
+++ petrinet-api/src/test/scala/com/ing/baker/petrinet/api/MarkingSpec.scala
@@ -1,69 +1,77 @@
 package com.ing.baker.petrinet.api
 
-import com.ing.baker.petrinet.dsl.Place
 import org.scalatest.Matchers._
 import org.scalatest.WordSpec
 
 class MarkingSpec extends WordSpec {
 
-  type MarkedPlace[P] = (P, MultiSet[Any])
-
   case class Person(name: String, age: Int)
 
-  val p1 = Place(id = 1)
-  val p2 = Place(id = 2)
-  val p3 = Place(id = 3)
-  val p4 = Place(id = 4)
+  type Place = Int
 
-  def createMarking[P](m1: MarkedPlace[P]): Marking[P] = {
-    Map[P, MultiSet[Any]](m1)
-  }
-
-  def createMarking[P](m1: MarkedPlace[P], m2: MarkedPlace[P]): Marking[P] = {
-    Map[P, MultiSet[Any]](m1, m2)
-  }
-
-  def createMarking[P](m1: MarkedPlace[P], m2: MarkedPlace[P], m3: MarkedPlace[P]): Marking[P] = {
-    Map[P, MultiSet[Any]](m1, m2, m3)
-  }
+  val p1 = 1
+  val p2 = 2
+  val p3 = 3
+  val p4 = 4
 
   "A Colored Marking" should {
 
     "correctly implement the multiplicity function" in {
 
-      val m = createMarking(p1(1, 2), p2("foo", "bar"))
+      val m: Marking[Place] = Map(
+        p1 -> MultiSet(1, 2),
+        p2 -> MultiSet("foo", "bar"))
 
       m.multiplicities shouldBe Map(p1 -> 2, p2 -> 2)
     }
 
     "have correct produce semantics" in {
 
-      val m1: Marking[Place] = createMarking(p1(1, 2), p2("foo", "bar"))
+      val m1: Marking[Place] = Map(
+        p1 -> MultiSet(1, 2),
+        p2 -> MultiSet("foo", "bar"))
 
       m1 |+| Marking.empty shouldBe m1
 
-      val m2: Marking[Place] = createMarking(p1(3), p2("baz"), p3(1d))
+      val m2: Marking[Place] = Map(
+        p1 -> MultiSet(3),
+        p2 -> MultiSet("baz"),
+        p3 -> MultiSet(1d))
 
-      m1 |+| m2 shouldBe createMarking(p1(3, 1, 2), p2("baz", "foo", "bar"), p3(1d))
+      val expected: Marking[Place] = Map(
+        p1 -> MultiSet(3, 1, 2),
+        p2 -> MultiSet("baz", "foo", "bar"),
+        p3 -> MultiSet(1d))
+
+      m1 |+| m2 shouldBe expected
     }
 
     "have correct consume semantics" in {
 
-      val m1: Marking[Place] = createMarking(p1(1, 2, 3), p2("foo", "bar"), p4(Person("Joe", 42)))
+      val m1: Marking[Place] = Map(
+        p1 -> MultiSet(1, 2, 3),
+        p2 -> MultiSet("foo", "bar"),
+        p4 -> MultiSet(Person("Joe", 42)))
 
       m1 |-| Marking.empty shouldBe m1
 
-      val m2: Marking[Place] = createMarking(p1(2), p4(Person("Joe", 42)))
+      val m2: Marking[Place] = Map(
+        p1 -> MultiSet(2),
+        p4 -> MultiSet(Person("Joe", 42)))
 
-      m1 |-| m2 shouldBe createMarking(p1(1, 3), p2("foo", "bar"))
+      val expected: Marking[Place] = Map(
+        p1 -> MultiSet(1, 3),
+        p2 -> MultiSet("foo", "bar"))
+
+      m1 |-| m2 shouldBe expected
     }
 
     "in case of token value equality only consume tokens equal to the multiplicity" in {
 
-      val m1 = createMarking(p1(1, 1, 1, 1, 1))
-      val m2 = createMarking(p1(1, 1))
+      val m1: Marking[Place] = Map(p1 -> MultiSet(1, 1, 1, 1, 1))
+      val m2: Marking[Place] = Map(p1 -> MultiSet(1, 1))
 
-      m1 |-| m2 shouldBe createMarking(p1(1, 1, 1))
+      m1 |-| m2 shouldBe Map(p1 -> MultiSet(1, 1, 1))
     }
   }
 }
