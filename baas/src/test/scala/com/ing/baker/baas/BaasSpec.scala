1d96a65c91ae494a85c5a361f11e965491a288ca
Tim Linschoten <Tim.Linschoten@ing.nl>
2019-02-05 11:39:08 +0100

    Extended the BaasSpec to also test error scenarios of interactions.

--- baas/src/test/scala/com/ing/baker/baas/BaasSpec.scala
+++ baas/src/test/scala/com/ing/baker/baas/BaasSpec.scala
@@ -4,13 +4,16 @@ import java.util.UUID
 
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
-import com.ing.baker.baas.BaasSpec.{InteractionOne, _}
+import com.ing.baker.baas.BaasSpec.{InteractionOne, InteractionTwo, _}
 import com.ing.baker.baas.server.BaasServer
 import com.ing.baker.compiler.RecipeCompiler
 import com.ing.baker.il.CompiledRecipe
+import com.ing.baker.recipe.javadsl.InteractionFailureStrategy.RetryWithIncrementalBackoffBuilder
 import com.ing.baker.recipe.scaladsl
 import com.ing.baker.recipe.scaladsl._
 import com.ing.baker.runtime.core.{AkkaBaker, Baker, BakerProvider, BakerResponse, ProcessState, RuntimeEvent, SensoryEventStatus}
+import org.mockito.Matchers.anyString
+import org.mockito.Mockito.{mock, reset, when}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.Await
@@ -29,11 +32,16 @@ class BaasSpec extends TestKit(ActorSystem("BAASSpec")) with WordSpecLike with M
   // Start a BAAS API
   val baasBaker: Baker = BakerProvider()
 
+  val mockOne = mock(classOf[InteractionOne])
+  val mockTwo = mock(classOf[InteractionTwo])
+
+
   // implementations
-  val localImplementations: Seq[AnyRef] = Seq(InteractionOne(), InteractionTwo())
+  val localImplementations: Seq[AnyRef] = Seq(mockOne, mockTwo)
 
 
   override def beforeAll() {
+    reset(mockOne, mockTwo)
     Await.result(baasAPI.start(), 10 seconds)
     baasBaker.addImplementations(localImplementations)
   }
@@ -43,6 +51,12 @@ class BaasSpec extends TestKit(ActorSystem("BAASSpec")) with WordSpecLike with M
   }
 
   "Happy flow simple recipe BAAS" in {
+    when(mockOne.apply(anyString(), anyString()))
+      .thenReturn(InteractionOneEvent("InteractionOneOutput"))
+
+    when(mockTwo.apply(anyString(), anyString()))
+      .thenThrow(new RuntimeException("InteractionTwoFailure"))
+      .thenReturn(InteractionTwoEvent("InteractionTwoOutput"))
 
     val recipeName = "simpleRecipe" + UUID.randomUUID().toString
     val recipe: Recipe = setupSimpleRecipe(recipeName)
@@ -61,6 +75,7 @@ class BaasSpec extends TestKit(ActorSystem("BAASSpec")) with WordSpecLike with M
 
     processState.ingredients.keys should contain("initialIngredient")
     processState.ingredients.keys should contain("interactionOneIngredient")
+    processState.ingredients.keys should contain("interactionTwoIngredient")
 
     val events: Seq[RuntimeEvent] = baasBaker.events(requestId)
 
@@ -74,6 +89,12 @@ class BaasSpec extends TestKit(ActorSystem("BAASSpec")) with WordSpecLike with M
   }
 
   "Process Event Async with http streaming" in {
+    when(mockOne.apply(anyString(), anyString()))
+      .thenReturn(InteractionOneEvent("InteractionOneOutput"))
+
+    when(mockTwo.apply(anyString(), anyString()))
+      .thenThrow(new RuntimeException("InteractionTwoFailure"))
+      .thenReturn(InteractionTwoEvent("InteractionTwoOutput"))
 
     val recipeName = "simpleRecipe" + UUID.randomUUID().toString
     val recipe: Recipe = setupSimpleRecipe(recipeName)
@@ -81,6 +102,7 @@ class BaasSpec extends TestKit(ActorSystem("BAASSpec")) with WordSpecLike with M
     val recipeId = baasBaker.addRecipe(compiledRecipe)
 
     val requestId = UUID.randomUUID().toString
+
 
     baasBaker.bake(recipeId, requestId)
 
@@ -99,8 +121,10 @@ object BaasSpec {
   val interactionOneIngredient = Ingredient[String]("interactionOneIngredient")
 
   val initialEvent = Event("InitialEvent", initialIngredient)
+
   case class InitialEvent(initialIngredient: String)
   case class InteractionOneEvent(interactionOneIngredient: String)
+  case class InteractionTwoEvent(interactionTwoIngredient: String)
 
   val interactionOne =
     Interaction(
@@ -108,26 +132,37 @@ object BaasSpec {
       inputIngredients = Seq(processId, initialIngredient),
       output = Seq(Event[InteractionOneEvent]))
 
-  case class InteractionOne() {
+  trait InteractionOne {
     def name: String = "InteractionOne"
-    def apply(processId: String, initialIngredient: String): InteractionOneEvent = {
-      println("Executing interactionOne")
-      InteractionOneEvent(initialIngredient)
-    }
+    def apply(processId: String, initialIngredient: String): InteractionOneEvent
   }
 
-  case class InteractionTwo() {
+  val interactionTwo =
+    Interaction(
+      name = "InteractionTwo",
+      inputIngredients = Seq(processId, initialIngredient),
+      output = Seq(Event[InteractionTwoEvent]))
+
+  trait InteractionTwo {
     def name: String = "InteractionTwo"
-    def apply(processId: String, initialIngredient: String): String = {
-      println("Executing InteractionTwo")
-      initialIngredient
-    }
+    def apply(processId: String, initialIngredient: String): InteractionTwoEvent
   }
 
   def setupSimpleRecipe(name: String): scaladsl.Recipe = {
     scaladsl.Recipe(name)
-      .withInteraction(interactionOne)
-      .withSensoryEvent(initialEvent)
+      .withInteractions(
+        interactionOne,
+        interactionTwo
+      )
+      .withSensoryEvent(
+        initialEvent
+      )
+      .withDefaultFailureStrategy(
+        new RetryWithIncrementalBackoffBuilder()
+          .withInitialDelay(java.time.Duration.ofMillis(10l))
+          .withMaximumRetries(10)
+          .build()
+      )
   }
 
 }
