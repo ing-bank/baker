0ce379541bcfcc4bd40b24030858d6b02181044b
Francisco Aramburo <francoara@pm.me>
2019-02-13 13:48:26 +0100

    Added dependencies for Akka Typed

---
+++ runtime/src/main/scala/com/ing/baker/runtime/actortyped/RecipeManager.scala
@@ -0,0 +1,9 @@
+package com.ing.baker.runtime.actortyped
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+
+object RecipeManager {
+
+}
