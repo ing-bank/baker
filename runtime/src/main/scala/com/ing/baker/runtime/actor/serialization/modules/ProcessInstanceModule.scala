beaf84423e9e3ec546bb6378f18cd71fde1105d5
Tim Linschoten <Tim.Linschoten@ing.nl>
2019-03-13 10:28:17 +0100

    made the removal of state from the TransitionFired backwards compatible.

--- runtime/src/main/scala/com/ing/baker/runtime/actor/serialization/modules/ProcessInstanceModule.scala
+++ runtime/src/main/scala/com/ing/baker/runtime/actor/serialization/modules/ProcessInstanceModule.scala
@@ -40,8 +40,7 @@ class ProcessInstanceModule extends ProtoEventAdapterModule {
       protobuf.TransitionNotEnabled(Some(transitionId), Some(reason))
     case protocol.TransitionFired(jobId, transitionId, correlationId, consumed, produced, newJobs, output) =>
       protobuf.TransitionFiredMessage(Some(jobId), Some(transitionId), correlationId,
-        toProtoMarking(consumed, ctx), toProtoMarking(produced, ctx),
-        newJobs.toSeq, Some(ctx.toProtoAny(output.asInstanceOf[AnyRef])))
+        toProtoMarking(consumed, ctx), toProtoMarking(produced, ctx), Option.empty, newJobs.toSeq, Some(ctx.toProtoAny(output.asInstanceOf[AnyRef])))
     case protocol.TransitionFailed(jobId, transitionId, correlationId, consume, input, reason, strategy) =>
       protobuf.TransitionFailedMessage(Some(jobId), Some(transitionId), correlationId, toProtoMarking(consume, ctx),
         Some(ctx.toProtoAny(input.asInstanceOf[AnyRef])), Some(reason), Some(ctx.toProto[protobuf.FailureStrategyMessage](strategy)))
@@ -108,7 +107,7 @@ class ProcessInstanceModule extends ProtoEventAdapterModule {
       protocol.AlreadyReceived(correlationId)
     case protobuf.TransitionNotEnabled(Some(transitionId), Some(reason)) =>
       protocol.TransitionNotEnabled(transitionId, reason)
-    case protobuf.TransitionFiredMessage(Some(jobId), Some(transitionId), correlationId, consumed, produced, newJobsIds, Some(output)) =>
+    case protobuf.TransitionFiredMessage(Some(jobId), Some(transitionId), correlationId, consumed, produced, _, newJobsIds, Some(output)) =>
       protocol.TransitionFired(jobId, transitionId, correlationId, toDomainMarking(consumed, ctx),
         toDomainMarking(produced, ctx), newJobsIds.toSet, ctx.toDomain[Any](output))
     case protobuf.TransitionFailedMessage(Some(jobId), Some(transitionId), correlationId, consume, Some(input), Some(reason), Some(strategy)) =>
