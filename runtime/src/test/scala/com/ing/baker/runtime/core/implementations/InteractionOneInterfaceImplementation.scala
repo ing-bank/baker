fc9ad59ae97615c7134c2cf521e731c38b94e13e
Francisco Aramburo <francoara@pm.me>
2019-04-25 14:27:08 +0200

    test of Scala async implementations

--- runtime/src/test/scala/com/ing/baker/runtime/core/implementations/InteractionOneInterfaceImplementation.scala
+++ runtime/src/test/scala/com/ing/baker/runtime/core/implementations/InteractionOneInterfaceImplementation.scala
@@ -3,6 +3,8 @@ package com.ing.baker.runtime.core.implementations
 import com.ing.baker.recipe.TestRecipe
 import com.ing.baker.recipe.TestRecipe.InteractionOneSuccessful
 
+import scala.concurrent.Future
+
 class InteractionOneInterfaceImplementation() extends TestRecipe.InteractionOne {
-  override def apply(processId: String, initialIngredient: String): InteractionOneSuccessful = InteractionOneSuccessful("")
+  override def apply(processId: String, initialIngredient: String): Future[InteractionOneSuccessful] = Future.successful(InteractionOneSuccessful(""))
 }
