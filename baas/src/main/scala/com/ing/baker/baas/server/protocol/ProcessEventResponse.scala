ee2006b834434071bf59f86900bab3dbeb991f62
Tim Linschoten <Tim.Linschoten@ing.nl>
2019-06-19 09:33:46 +0200

    Fixed the SensoryEventStatus proto message that was in two seperate protobuf files.

--- baas/src/main/scala/com/ing/baker/baas/server/protocol/ProcessEventResponse.scala
+++ baas/src/main/scala/com/ing/baker/baas/server/protocol/ProcessEventResponse.scala
@@ -1,10 +1,10 @@
 package com.ing.baker.baas.server.protocol
 
 import com.ing.baker.runtime.akka.actor.serialization.ProtoMap
-import com.ing.baker.runtime.akka.actor.serialization.ProtoMap.{ctxFromProto, ctxToProto, versioned}
+import com.ing.baker.runtime.akka.actor.serialization.ProtoMap.versioned
+import com.ing.baker.runtime.akka.actor.serialization.protomappings.SensoryEventStatusMappingHelper
 import com.ing.baker.runtime.baas.protobuf
 import com.ing.baker.runtime.common.SensoryEventStatus
-import com.ing.baker.runtime.{common, akka}
 import scalapb.GeneratedMessageCompanion
 
 import scala.util.Try
@@ -20,34 +20,11 @@ object ProcessEventResponse {
         protobuf.ProcessEventResponse
 
       override def toProto(a: ProcessEventResponse): protobuf.ProcessEventResponse =
-        protobuf.ProcessEventResponse(Some(toSensoryEventStatusProto(a.status)))
+        protobuf.ProcessEventResponse(Some(SensoryEventStatusMappingHelper.toProto(a.status)))
 
       override def fromProto(message: protobuf.ProcessEventResponse): Try[ProcessEventResponse] =
         for {
           status <- versioned(message.sensoryEventStatus, "sensoryEventStatus")
-        } yield ProcessEventResponse(fromSensoryEventStatusProto(status))
+        } yield ProcessEventResponse(SensoryEventStatusMappingHelper.fromProto(status).get)
     }
-
-  private def toSensoryEventStatusProto(sensoryEventStatus: SensoryEventStatus): protobuf.SensoryEventStatus = {
-    sensoryEventStatus match {
-      case common.SensoryEventStatus.Received => protobuf.SensoryEventStatus.Received
-      case common.SensoryEventStatus.AlreadyReceived => protobuf.SensoryEventStatus.AlreadyReceived
-      case common.SensoryEventStatus.Completed => protobuf.SensoryEventStatus.Completed
-      case common.SensoryEventStatus.FiringLimitMet => protobuf.SensoryEventStatus.FiringLimitMet
-      case common.SensoryEventStatus.ProcessDeleted => protobuf.SensoryEventStatus.ProcessDeleted
-      case common.SensoryEventStatus.ReceivePeriodExpired => protobuf.SensoryEventStatus.ReceivePeriodExpired
-    }
-  }
-
-  private def fromSensoryEventStatusProto(sensoryEventStatus: protobuf.SensoryEventStatus): SensoryEventStatus = {
-    sensoryEventStatus match {
-      case protobuf.SensoryEventStatus.Received => common.SensoryEventStatus.Received
-      case protobuf.SensoryEventStatus.AlreadyReceived => common.SensoryEventStatus.AlreadyReceived
-      case protobuf.SensoryEventStatus.Completed => common.SensoryEventStatus.Completed
-      case protobuf.SensoryEventStatus.FiringLimitMet => common.SensoryEventStatus.FiringLimitMet
-      case protobuf.SensoryEventStatus.ProcessDeleted => common.SensoryEventStatus.ProcessDeleted
-      case protobuf.SensoryEventStatus.ReceivePeriodExpired => common.SensoryEventStatus.ReceivePeriodExpired
-      case _ =>  throw new IllegalArgumentException("Invalid SensoryEventStatus received during deserialisation")
-    }
-  }
 }
