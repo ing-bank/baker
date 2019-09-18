296c280787cc4d86d2c7e70ebeea4a60afccef9e
Tim Linschoten <Tim.Linschoten@ing.nl>
2019-09-18 17:08:39 +0200

    Added timeout for how long a agent can be considering. Removed the committed state and go into receive state directly.

--- runtime/src/main/scala/com/ing/baker/runtime/akka/actor/interaction_schedulling/InteractionAgent.scala
+++ runtime/src/main/scala/com/ing/baker/runtime/akka/actor/interaction_schedulling/InteractionAgent.scala
@@ -4,14 +4,17 @@ import java.util.UUID
 
 import akka.actor.{Actor, ActorRef, Props}
 import akka.cluster.pubsub.{DistributedPubSub, DistributedPubSubMediator}
+import com.ing.baker.runtime.akka.actor.interaction_schedulling.InteractionAgent._
 import com.ing.baker.runtime.scaladsl.{EventInstance, InteractionInstance}
 import org.slf4j.LoggerFactory
-import InteractionAgent._
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
+import scala.concurrent.duration._
 
 object InteractionAgent {
+
+  case object CommitTimeout
 
   def apply(instance: InteractionInstance): Props =
     Props(new InteractionAgent(instance))
@@ -22,16 +25,16 @@ object InteractionAgent {
     *
     * TODO: Handle invalid ingredients scenario
     *
-    * @param agent actor reference
+    * @param agent  actor reference
     * @param result outcome of invoking the interaction instance
-    * @param ec execution context to use
+    * @param ec     execution context to use
     */
-  private[interaction_schedulling] def pipeBackExecutionResponse(agent: ActorRef)(result: Future[Option[EventInstance]])(implicit ec: ExecutionContext): Unit = {
+  private[interaction_schedulling] def pipeBackExecutionResponse(agent: ActorRef, mandated: ActorRef)(result: Future[Option[EventInstance]])(implicit ec: ExecutionContext): Unit = {
     result.onComplete {
       case Success(value) =>
-        agent.tell(ProtocolInteractionExecution.InstanceExecutedSuccessfully(value), agent)
+        mandated.tell(ProtocolInteractionExecution.InstanceExecutedSuccessfully(value), agent)
       case Failure(exception) =>
-        agent.tell(ProtocolInteractionExecution.InstanceExecutionFailed(), agent)
+        mandated.tell(ProtocolInteractionExecution.InstanceExecutionFailed(), agent)
     }
   }
 
@@ -68,6 +71,7 @@ class InteractionAgent(interaction: InteractionInstance) extends Actor {
       log.info(s"${interaction.name}:$uuid: Considering quest from $mandated")
       mandated ! ProtocolQuestCommit.Considering(self)
       unsubscribePush()
+      context.system.scheduler.scheduleOnce(10 seconds, self, CommitTimeout)(context.dispatcher, self)
       context.become(considering(uuid))
 
     case ProtocolPushPullMatching.AvailableQuest(mandated, uuid) =>
@@ -75,6 +79,7 @@ class InteractionAgent(interaction: InteractionInstance) extends Actor {
       log.info(s"${interaction.name}:$uuid: Considering quest from $mandated")
       mandated ! ProtocolQuestCommit.Considering(self)
       unsubscribePush()
+      context.system.scheduler.scheduleOnce(10 seconds, self, CommitTimeout)(context.dispatcher, self)
       context.become(considering(uuid))
   }
 
@@ -82,8 +87,11 @@ class InteractionAgent(interaction: InteractionInstance) extends Actor {
     case ProtocolQuestCommit.Commit(mandated, executeMessage) =>
       log.info(s"${interaction.name}:$uuid: Commited to quest from $mandated")
       // start the execution protocol by already starting the computation and become committed
-      InteractionAgent.pipeBackExecutionResponse(self)(interaction.run(executeMessage.input))
-      context.become(committed(mandated, uuid))
+      InteractionAgent.pipeBackExecutionResponse(self, mandated)(interaction.run(executeMessage.input))
+      subscribePush()
+      pull()
+      context.become(receive)
+    //      context.become(committed(mandated, uuid))
 
     case ProtocolQuestCommit.QuestTaken =>
       log.info(s"${interaction.name}:$uuid: Quest taken, starting the protocol again")
@@ -91,16 +99,23 @@ class InteractionAgent(interaction: InteractionInstance) extends Actor {
       subscribePush()
       pull()
       context.become(receive)
-  }
 
-  def committed(mandated: ActorRef, uuid: UUID): Receive = {
-    case message: ProtocolInteractionExecution =>
-      log.info(s"${interaction.name}:$uuid: Considering quest from $mandated")
-      // Forward the result
-      mandated ! message
-      // Start all over again
+    case CommitTimeout =>
+      log.info(s"${interaction.name}:$uuid: not received a response after commit timeout")
       subscribePush()
       pull()
       context.become(receive)
+
   }
+
+//  def committed(mandated: ActorRef, uuid: UUID): Receive = {
+//    case message: ProtocolInteractionExecution =>
+//      log.info(s"${interaction.name}:$uuid: Considering quest from $mandated")
+//      // Forward the result
+//      mandated ! message
+//      // Start all over again
+//      subscribePush()
+//      pull()
+//      context.become(receive)
+//  }
 }
