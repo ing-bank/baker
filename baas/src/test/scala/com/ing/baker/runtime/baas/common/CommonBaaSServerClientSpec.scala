7058c4459788a6b87daa59b661b4e275998ec969
Francisco Aramburo <francoara@pm.me>
2019-09-30 16:22:42 +0200

    Continued work on the testing of the BaaS client-server communication

--- baas/src/test/scala/com/ing/baker/runtime/baas/common/CommonBaaSServerClientSpec.scala
+++ baas/src/test/scala/com/ing/baker/runtime/baas/common/CommonBaaSServerClientSpec.scala
@@ -1,57 +1,140 @@
 package com.ing.baker.runtime.baas.common
 
+import java.util.UUID
+
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.stream.Materializer
+import akka.stream.{ActorMaterializer, Materializer}
 import cats.effect.{IO, Timer}
-import cats.~>
 import com.ing.baker.compiler.RecipeCompiler
 import com.ing.baker.runtime.baas.BaaSServer
-import com.ing.baker.runtime.common.Baker
+import com.ing.baker.runtime.baas.common.CheckoutFlowIngredients.ShippingAddress
+import com.ing.baker.runtime.baas.common.CommonBaaSServerClientSpec.ClientServerTest
 import com.ing.baker.runtime.common.LanguageDataStructures.LanguageApi
-import com.ing.baker.runtime.scaladsl.{InteractionInstance, Baker => ScalaBaker}
-import org.scalatest.{AsyncFunSpec, BeforeAndAfterAll, Matchers}
+import com.ing.baker.runtime.common.SensoryEventStatus
+import com.ing.baker.runtime.scaladsl.{EventInstance, InteractionInstance, Baker => ScalaBaker}
+import org.scalatest.compatible.Assertion
+import org.scalatest.{AsyncFunSpec, BeforeAndAfterAll, BeforeAndAfterEach, Matchers}
 
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{ExecutionContext, Future}
 
-abstract class CommonBaaSServerClientSpec[F[_]](
-                                                 clientBaker: Baker[F],
-                                                 serverBaker: ScalaBaker,
-                                                 host: String,
-                                                 port: Int,
-                                                 toFuture: F ~> Future)(
-                                                 implicit system: ActorSystem, materializer: Materializer
-                                               )
+abstract class CommonBaaSServerClientSpec(clientBaker: (String, ActorSystem, Materializer) => ScalaBaker)
   extends AsyncFunSpec
     with Matchers
     with BeforeAndAfterAll
+    with BeforeAndAfterEach
     with LanguageApi {
 
-  var serverShutdown: Http.ServerBinding = _
+  val test: ClientServerTest = CommonBaaSServerClientSpec.testWith(clientBaker)
 
-  implicit val timer: Timer[IO] = IO.timer(system.dispatcher)
+  describe("Baker Client-Server") {
+    it("Baker.addRecipe") {
+      test { (client, server) =>
+        val compiledRecipe = RecipeCompiler.compileRecipe(CheckoutFlowRecipe.recipe)
+        for {
+          recipeId <- client.addRecipe(compiledRecipe)
+          recipeInformation <- server.getRecipe(recipeId)
+        } yield recipeInformation.compiledRecipe shouldBe compiledRecipe
+      }
+    }
 
-  override def beforeAll(): Unit = {
+    it("Baker.getRecipe") {
+      test { (client, _) =>
+        val compiledRecipe = RecipeCompiler.compileRecipe(CheckoutFlowRecipe.recipe)
+        for {
+          recipeId <- client.addRecipe(compiledRecipe)
+          recipeInformation <- client.getRecipe(recipeId)
+        } yield recipeInformation.compiledRecipe shouldBe compiledRecipe
+      }
+    }
+
+    it("Baker.getAllRecipes") {
+      test { (client, _) =>
+        val compiledRecipe = RecipeCompiler.compileRecipe(CheckoutFlowRecipe.recipe)
+        for {
+          recipeId <- client.addRecipe(compiledRecipe)
+          recipes <- client.getAllRecipes
+        } yield recipes.get(recipeId).map(_.compiledRecipe) shouldBe Some(compiledRecipe)
+      }
+    }
+
+    it("Baker.bake") {
+      test { (client, server) =>
+        val compiledRecipe = RecipeCompiler.compileRecipe(CheckoutFlowRecipe.recipe)
+        val recipeInstanceId: String = UUID.randomUUID().toString
+        for {
+          recipeId <- client.addRecipe(compiledRecipe)
+          _ <- client.bake(recipeId, recipeInstanceId)
+          state <- server.getRecipeInstanceState(recipeInstanceId)
+        } yield state.recipeInstanceId shouldBe recipeInstanceId
+      }
+    }
+
+    it("Baker.fireEventAndResolveWhenReceived") {
+      test { (client, server) =>
+        val compiledRecipe = RecipeCompiler.compileRecipe(CheckoutFlowRecipe.recipe)
+        val recipeInstanceId: String = UUID.randomUUID().toString
+        val event = EventInstance.unsafeFrom(
+          CheckoutFlowEvents.ShippingAddressReceived(ShippingAddress("address")))
+        for {
+          recipeId <- client.addRecipe(compiledRecipe)
+          _ <- client.bake(recipeId, recipeInstanceId)
+          status <- client.fireEventAndResolveWhenReceived(recipeInstanceId, event)
+        } yield status shouldBe SensoryEventStatus.Received
+      }
+    }
+
+    it("Baker.fireEventAndResolveWhenCompleted") {
+      test { (client, server) =>
+        val compiledRecipe = RecipeCompiler.compileRecipe(CheckoutFlowRecipe.recipe)
+        val recipeInstanceId: String = UUID.randomUUID().toString
+        val event = EventInstance.unsafeFrom(
+          CheckoutFlowEvents.ShippingAddressReceived(ShippingAddress("address")))
+        for {
+          recipeId <- client.addRecipe(compiledRecipe)
+          _ <- client.bake(recipeId, recipeInstanceId)
+          result <- client.fireEventAndResolveWhenCompleted(recipeInstanceId, event)
+        } yield result.eventNames should contain("ShippingAddressReceived")
+      }
+    }
+  }
+}
+
+object CommonBaaSServerClientSpec {
+
+  type ClientServerTest = ((ScalaBaker, ScalaBaker) => Future[Assertion]) => Future[Assertion]
+
+  val allPorts: Stream[Int] = Stream.from(50000, 1)
+
+  def testWith[F[_], Lang <: LanguageApi]
+      (clientBaker: (String, ActorSystem, Materializer) => ScalaBaker)
+      (t: (ScalaBaker, ScalaBaker) => Future[Assertion])
+      (implicit ec: ExecutionContext): Future[Assertion] = {
+    val testId: UUID = UUID.randomUUID()
+    implicit val system: ActorSystem = ActorSystem("ScalaDSLBaaSServerClientSpec-" + testId)
+    implicit val materializer: Materializer = ActorMaterializer()
+    implicit val timer: Timer[IO] = IO.timer(system.dispatcher)
+    val host: String = "localhost"
+    val serverBaker = ScalaBaker.akkaLocalDefault(system, materializer)
     val makePaymentInstance: InteractionInstance = InteractionInstance.unsafeFrom(new MakePaymentInstance())
     val reserveItemsInstance: InteractionInstance = InteractionInstance.unsafeFrom(new ReserveItemsInstance())
     val shipItemsInstance: InteractionInstance = InteractionInstance.unsafeFrom(new ShipItemsInstance())
-    val addInteractions = serverBaker.addInteractionInstances(Seq(makePaymentInstance, reserveItemsInstance, shipItemsInstance))
-    Await.result(addInteractions, 10 seconds)
-    serverShutdown = Await.result(BaaSServer.run(serverBaker, host, port), 10 seconds)
+    for {
+      _ <- serverBaker.addInteractionInstances(Seq(makePaymentInstance, reserveItemsInstance, shipItemsInstance))
+      (client, shutdown) <- createClientServerPair(allPorts, { port =>
+        val client = clientBaker(s"http://$host:$port/", system, materializer)
+        val shutdown = BaaSServer.run(serverBaker, host, port)
+        shutdown.map(s => (client, s))
+      })
+      a <- t(client, serverBaker)
+      _ <- shutdown.unbind()
+      _ <- serverBaker.gracefulShutdown()
+    } yield a
   }
 
-  override def afterAll(): Unit = {
-    Await.result(serverShutdown.unbind(), 10 seconds)
-  }
-
-  describe("Baker Client-Server comm") {
-    it("Baker.addRecipe") {
-      val compiledRecipe = RecipeCompiler.compileRecipe(CheckoutFlowRecipe.recipe)
-      for {
-        recipeId <- toFuture(clientBaker.addRecipe(compiledRecipe))
-        recipeInformation <- serverBaker.getRecipe(recipeId)
-      } yield recipeInformation.compiledRecipe shouldBe compiledRecipe
+  private def createClientServerPair[T](ports: Stream[Int], buildServer: Int => Future[T])(implicit ec: ExecutionContext): Future[T] =
+    ports match {
+      case #::(port, tail) => buildServer(port).recoverWith {
+        case _: java.net.BindException => createClientServerPair(tail, buildServer)
+      }
     }
-  }
 }
