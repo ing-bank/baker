e81c5cd95ec95f274b5c435b735c123325d5da1e
Francisco Aramburo <francoara@pm.me>
2019-10-04 15:45:53 +0200

    finished the serialization of BakerExceptions

--- baas/src/main/scala/com/ing/baker/runtime/baas/BaaSServer.scala
+++ baas/src/main/scala/com/ing/baker/runtime/baas/BaaSServer.scala
@@ -4,11 +4,13 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{RequestContext, Route}
 import akka.stream.Materializer
 import com.ing.baker.runtime.akka.actor.serialization.{Encryption, SerializersProvider}
 import com.ing.baker.runtime.baas.BaaSProto._
+import com.ing.baker.runtime.baas.BaaSProtocol.BaaSRemoteFailure
 import com.ing.baker.runtime.baas.MarshallingUtils._
+import com.ing.baker.runtime.common.BakerException
 import com.ing.baker.runtime.scaladsl.Baker
 
 import scala.concurrent.Future
@@ -37,45 +39,43 @@ class BaaSServer(implicit system: ActorSystem, mat: Materializer, baker: Baker, 
 
   private def addRecipe: Route = post(path("addRecipe") {
     entity(as[BaaSProtocol.AddRecipeRequest]) { request =>
-      complete(baker.addRecipe(request.compiledRecipe).map(BaaSProtocol.AddRecipeResponse))
+      completeWithBakerFailures(baker.addRecipe(request.compiledRecipe).map(BaaSProtocol.AddRecipeResponse))
     }
   })
 
   private def getRecipe: Route = post(path("getRecipe") {
     entity(as[BaaSProtocol.GetRecipeRequest]) { request =>
-      complete(baker.getRecipe(request.recipeId).map(BaaSProtocol.GetRecipeResponse))
+      completeWithBakerFailures(baker.getRecipe(request.recipeId).map(BaaSProtocol.GetRecipeResponse))
     }
   })
 
   private def getAllRecipes: Route = post(path("getAllRecipes") {
-      complete(baker.getAllRecipes.map(BaaSProtocol.GetAllRecipesResponse))
+      completeWithBakerFailures(baker.getAllRecipes.map(BaaSProtocol.GetAllRecipesResponse))
   })
 
   private def bake: Route = post(path("bake") {
     entity(as[BaaSProtocol.BakeRequest]) { request =>
-      onComplete(baker.bake(request.recipeId, request.recipeInstanceId)) { _ =>
-        complete(StatusCodes.OK)
-      }
+      completeWithBakerFailures(baker.bake(request.recipeId, request.recipeInstanceId))
     }
   })
 
   private def fireEventAndResolveWhenReceived: Route = post(path("fireEventAndResolveWhenReceived") {
     entity(as[BaaSProtocol.FireEventAndResolveWhenReceivedRequest]) { request =>
-      complete(baker.fireEventAndResolveWhenReceived(request.recipeInstanceId, request.event, request.correlationId)
+      completeWithBakerFailures(baker.fireEventAndResolveWhenReceived(request.recipeInstanceId, request.event, request.correlationId)
         .map(BaaSProtocol.FireEventAndResolveWhenReceivedResponse))
     }
   })
 
   private def fireEventAndResolveWhenCompleted: Route = post(path("fireEventAndResolveWhenCompleted") {
     entity(as[BaaSProtocol.FireEventAndResolveWhenCompletedRequest]) { request =>
-      complete(baker.fireEventAndResolveWhenCompleted(request.recipeInstanceId, request.event, request.correlationId)
+      completeWithBakerFailures(baker.fireEventAndResolveWhenCompleted(request.recipeInstanceId, request.event, request.correlationId)
         .map(BaaSProtocol.FireEventAndResolveWhenCompletedResponse))
     }
   })
 
   private def fireEventAndResolveOnEvent: Route = post(path("fireEventAndResolveOnEvent") {
     entity(as[BaaSProtocol.FireEventAndResolveOnEventRequest]) { request =>
-      complete(baker.fireEventAndResolveOnEvent(request.recipeInstanceId, request.event, request.onEvent, request.correlationId)
+      completeWithBakerFailures(baker.fireEventAndResolveOnEvent(request.recipeInstanceId, request.event, request.onEvent, request.correlationId)
         .map(BaaSProtocol.FireEventAndResolveOnEventResponse))
     }
   })
@@ -88,45 +88,39 @@ class BaaSServer(implicit system: ActorSystem, mat: Materializer, baker: Baker, 
   })
 
   private def getAllRecipeInstancesMetadata: Route = post(path("getAllRecipeInstancesMetadata") {
-      complete(baker.getAllRecipeInstancesMetadata
+      completeWithBakerFailures(baker.getAllRecipeInstancesMetadata
         .map(BaaSProtocol.GetAllRecipeInstancesMetadataResponse))
   })
 
   private def getRecipeInstanceState: Route = post(path("getRecipeInstanceState") {
     entity(as[BaaSProtocol.GetRecipeInstanceStateRequest]) { request =>
-      complete(baker.getRecipeInstanceState(request.recipeInstanceId)
+      completeWithBakerFailures(baker.getRecipeInstanceState(request.recipeInstanceId)
         .map(BaaSProtocol.GetRecipeInstanceStateResponse))
     }
   })
 
   private def getVisualState: Route = post(path("getVisualState") {
     entity(as[BaaSProtocol.GetVisualStateRequest]) { request =>
-      complete(baker.getVisualState(request.recipeInstanceId)
+      completeWithBakerFailures(baker.getVisualState(request.recipeInstanceId)
         .map(BaaSProtocol.GetVisualStateResponse))
     }
   })
 
   private def retryInteraction: Route = post(path("retryInteraction") {
     entity(as[BaaSProtocol.RetryInteractionRequest]) { request =>
-      onComplete(baker.retryInteraction(request.recipeInstanceId, request.interactionName)) { _ =>
-        complete(StatusCodes.OK)
-      }
+      completeWithBakerFailures(baker.retryInteraction(request.recipeInstanceId, request.interactionName))
     }
   })
 
   private def resolveInteraction: Route = post(path("resolveInteraction") {
     entity(as[BaaSProtocol.ResolveInteractionRequest]) { request =>
-      onComplete(baker.resolveInteraction(request.recipeInstanceId, request.interactionName, request.event)) { _ =>
-        complete(StatusCodes.OK)
-      }
+      completeWithBakerFailures(baker.resolveInteraction(request.recipeInstanceId, request.interactionName, request.event))
     }
   })
 
   private def stopRetryingInteraction: Route = post(path("stopRetryingInteraction") {
     entity(as[BaaSProtocol.StopRetryingInteractionRequest]) { request =>
-      onComplete(baker.stopRetryingInteraction(request.recipeInstanceId, request.interactionName)) { _ =>
-        complete(StatusCodes.OK)
-      }
+      completeWithBakerFailures(baker.stopRetryingInteraction(request.recipeInstanceId, request.interactionName))
     }
   })
 
--- runtime/src/main/scala/com/ing/baker/runtime/common/BakerException.scala
+++ runtime/src/main/scala/com/ing/baker/runtime/common/BakerException.scala
@@ -1,15 +1,56 @@
 package com.ing.baker.runtime.common
 
-sealed abstract class BakerException(message: String = "An exception occurred at Baker", cause: Throwable = null)
+import scala.util.{Failure, Success, Try}
+
+sealed abstract class BakerException(val message: String = "An exception occurred at Baker", val enum: Int, val cause: Throwable = null)
     extends RuntimeException(message, cause)
 
 object BakerException {
 
-  class NoSuchProcessException(message: String) extends BakerException(message)
+  // TODO this has to be renamed to NoSuchRecipeInstanceException
+  case class NoSuchProcessException(recipeInstanceId: String)
+    extends BakerException(s"Recipe instance $recipeInstanceId does not exist in the index", 1)
 
-  class ProcessDeletedException(message: String) extends BakerException(message)
+  // TODO this has to be renamed to RecipeInstanceDeletedException
+  case class ProcessDeletedException(recipeInstanceId: String)
+    extends BakerException(s"Recipe instance $recipeInstanceId is deleted", 2)
 
-  class RecipeValidationException(message: String) extends BakerException(message)
+  case class RecipeValidationException(validationErrors: String)
+    extends BakerException(validationErrors, 3)
 
-  class ImplementationsException(message: String) extends BakerException(message)
+  case class ImplementationsException(implementationErrors: String)
+    extends BakerException(implementationErrors, 4)
+
+  case class NoSuchRecipeException(recipeId: String)
+    extends BakerException(s"No recipe found for recipe with id: $recipeId", 5)
+
+  // TODO this has to be renamed to RecipeInstanceAlreadyExistsException
+  case class ProcessAlreadyExistsException(recipeInstanceId: String)
+    extends BakerException(s"Process '$recipeInstanceId' already exists.", 6)
+
+  case class IllegalEventException(reason: String)
+    extends BakerException(reason, 7)
+
+  def encode(bakerException: BakerException): (String, Int) =
+    bakerException match {
+      case e @ NoSuchProcessException(recipeInstanceId) => (recipeInstanceId, e.enum)
+      case e @ ProcessDeletedException(recipeInstanceId) => (recipeInstanceId, e.enum)
+      case e @ RecipeValidationException(validationErrors) => (validationErrors, e.enum)
+      case e @ ImplementationsException(implementationErrors) => (implementationErrors, e.enum)
+      case e @ NoSuchRecipeException(recipeId) => (recipeId, e.enum)
+      case e @ ProcessAlreadyExistsException(recipeInstanceId) => (recipeInstanceId, e.enum)
+      case e @ IllegalEventException(reason) => (reason, e.enum)
+    }
+
+  def decode(message: String, enum: Int): Try[BakerException] =
+    enum match {
+      case 1 => Success(NoSuchProcessException(message))
+      case 2 => Success(ProcessDeletedException(message))
+      case 3 => Success(RecipeValidationException(message))
+      case 4 => Success(ImplementationsException(message))
+      case 5 => Success(NoSuchRecipeException(message))
+      case 6 => Success(ProcessAlreadyExistsException(message))
+      case 7 => Success(IllegalEventException(message))
+      case _ => Failure(new IllegalArgumentException(s"No BakerException with enum flag $enum"))
+    }
 }
