7b04648869b706b8c9dc8241a23eba204e52eb23
Francisco Aramburo <francoara@pm.me>
2019-10-23 16:36:47 +0200

    WIP baas project separation

--- baas/src/test/scala/com/ing/baker/runtime/baas/scaladsl/ScalaDSLBaaSServerClientSpec.scala
+++
@@ -1,9 +0,0 @@
-package com.ing.baker.runtime.baas.scaladsl
-
-import com.ing.baker.runtime.akka.actor.serialization.Encryption
-import com.ing.baker.runtime.baas.common.CommonBaaSServerClientSpec
-
-class ScalaDSLBaaSServerClientSpec extends CommonBaaSServerClientSpec(
-  (host, as, mat) => Baker.remote(host, Encryption.NoEncryption)(as, mat)
-)
-
