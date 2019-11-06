cefc9de5e75ba4c9de060a1a3db506bd061fe739
Scala Steward <me@scala-steward.org>
2019-11-06 02:25:12 +0100

    Update ficus to 1.4.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
   val levelDBJni =                "org.fusesource.leveldbjni"  %  "leveldbjni-all"                     % "1.8"
 
   val logback =                   "ch.qos.logback"             %  "logback-classic"                    % "1.2.3"
-  val ficusConfig =               "com.iheart"                 %% "ficus"                              % "1.4.0"
+  val ficusConfig =               "com.iheart"                 %% "ficus"                              % "1.4.7"
 
   val scalaGraph  =               "org.scala-graph"            %% "graph-core"                         % "1.11.5"
   val scalaGraphDot =             "org.scala-graph"            %% "graph-dot"                          % "1.11.5"
