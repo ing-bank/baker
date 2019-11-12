02dd87ee48278fff6c6a98360a2cc10beafec901
Nikola Kasev <nikola.kasev@gmail.com>
2019-11-12 19:20:51 +0100

    test done

--- runtime/src/test/scala/com/ing/baker/runtime/akka/internal/RecipeRuntimeSpec.scala
+++ runtime/src/test/scala/com/ing/baker/runtime/akka/internal/RecipeRuntimeSpec.scala
@@ -2,31 +2,32 @@ package com.ing.baker.runtime.akka.internal
 
 import java.util.UUID
 
-import com.ing.baker.il
 import com.ing.baker.il.IngredientDescriptor
 import com.ing.baker.il.petrinet.InteractionTransition
 import com.ing.baker.runtime.scaladsl.RecipeInstanceState
 import com.ing.baker.types.Value
+import com.ing.baker.{il, types}
 import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{Matchers, WordSpecLike}
 
 class RecipeRuntimeSpec extends WordSpecLike with Matchers with MockitoSugar {
   "The recipe runtime" should {
-    "return a ProcessID ingredient if necessary" in {
+    "provide a ProcessID ingredient to an interaction if required" in {
       val processId = UUID.randomUUID().toString
-      val notRelevant = Map.empty[String, Value]
+      val irrelevant = Map.empty[String, Value]
       val mockTransition = mock[InteractionTransition]
       val mockState = mock[RecipeInstanceState]
-      when(mockTransition.predefinedParameters).thenReturn(notRelevant)
-      // annotating an interaction with @ProcessId will include an extra ingredient in the list of required ingredients
-      //TODO how to turn the String into Type?
-//      when(mockTransition.requiredIngredients).thenReturn(Seq(IngredientDescriptor(il.processIdName, processId)))
-      when(mockState.ingredients).thenReturn(notRelevant)
-      when(mockState.recipeInstanceId).thenReturn(processId)
-      RecipeRuntime.createInteractionInput(mockTransition, mockState)
 
-      //TODO verify the id matches the processId
+      when(mockTransition.predefinedParameters).thenReturn(irrelevant)
+      when(mockState.ingredients).thenReturn(irrelevant)
+      //the @ProcessId annotation will include an extra ingredient in the list of required ingredients
+      when(mockTransition.requiredIngredients).thenReturn(Seq(IngredientDescriptor(il.processIdName, types.CharArray)))
+      //in V3, process id from V1 and V2 is now called a recipe instance id
+      when(mockState.recipeInstanceId).thenReturn(processId)
+
+      //this call would fail without the fix
+      RecipeRuntime.createInteractionInput(mockTransition, mockState)
     }
   }
 }
