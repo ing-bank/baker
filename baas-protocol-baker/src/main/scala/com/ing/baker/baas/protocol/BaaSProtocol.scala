a30b17282289f53a2efd17d136d41fed32d12893
yk24na <stas.shevchenko@ing.com>
2019-12-10 13:58:14 +0100

    Remove unused code

--- baas-protocol-baker/src/main/scala/com/ing/baker/baas/protocol/BaaSProtocol.scala
+++ baas-protocol-baker/src/main/scala/com/ing/baker/baas/protocol/BaaSProtocol.scala
@@ -16,13 +16,9 @@ object BaaSProtocol {
 
   case class GetRecipeResponse(recipeInformation: RecipeInformation)
 
-  //case class GetAllRecipesRequest()
-
   case class GetAllRecipesResponse(map: Map[String, RecipeInformation])
 
   case class BakeRequest(recipeId: String, recipeInstanceId: String)
-
-  //case class BakeResponse()
 
   case class FireEventAndResolveWhenReceivedRequest(recipeInstanceId: String, event: EventInstance, correlationId: Option[String])
 
@@ -39,9 +35,6 @@ object BaaSProtocol {
   case class FireEventRequest(recipeInstanceId: String, event: EventInstance, correlationId: Option[String])
 
   // case class FireEventResponse() TODO figure out how to deal with this one
-  //def fireEvent(recipeInstanceId: String, event: EventInstance, correlationId: Option[String]): EventResolutions = ???
-
-  //case class GetAllRecipeInstancesMetadataRequest()
 
   case class GetAllRecipeInstancesMetadataResponse(set: Set[RecipeInstanceMetadata])
 
@@ -49,25 +42,13 @@ object BaaSProtocol {
 
   case class GetRecipeInstanceStateResponse(recipeInstanceState: RecipeInstanceState)
 
-  //case class GetIngredientsRequest(recipeInstanceId: String)
-  //case class GetIngredientsResponse(map: Map[String, Value])
-  //case class GetEventsRequest(re)
-  //def getEvents(recipeInstanceId: String): Future[Seq[EventMoment]] = ???
-  //def getEventNames(recipeInstanceId: String): Future[Seq[String]] = ???
-
   case class GetVisualStateRequest(recipeInstanceId: String)
 
   case class GetVisualStateResponse(state: String)
 
   case class RetryInteractionRequest(recipeInstanceId: String, interactionName: String)
 
-  //case class RetryInteractionResponse()
-
   case class ResolveInteractionRequest(recipeInstanceId: String, interactionName: String, event: EventInstance)
 
-  // case class ResolveInteractionResponse()
-
   case class StopRetryingInteractionRequest(recipeInstanceId: String, interactionName: String)
-
-  // response
 }
