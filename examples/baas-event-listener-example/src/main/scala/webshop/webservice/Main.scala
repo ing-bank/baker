ddefe5385aaa1986191041ef5527719edf24c9d8
yk24na <stas.shevchenko@ing.com>
2019-12-10 16:43:14 +0100

    Remove println statement.

--- examples/baas-event-listener-example/src/main/scala/webshop/webservice/Main.scala
+++ examples/baas-event-listener-example/src/main/scala/webshop/webservice/Main.scala
@@ -2,13 +2,14 @@ package webshop.webservice
 
 import akka.actor.ActorSystem
 import com.ing.baker.baas.scaladsl.BaaSEventListener
+import com.typesafe.scalalogging.LazyLogging
 
-object Main extends App {
+object Main extends App with LazyLogging {
 
-  val actorSystem = ActorSystem("BaaS") // This should be done by the BaaSInteractionInstance ecosystem to ease the configuration and improve the UX
+  val actorSystem = ActorSystem("BaaS") // TODO: This should be done by the BaaSInteractionInstance ecosystem to ease the configuration and improve the UX
   val ecosystem = BaaSEventListener(actorSystem)
 
   ecosystem.registerEventListener("Webshop", (metadata, event) => {
-    println(metadata.recipeName + " [" + metadata.recipeInstanceId + "] " + event.name)
+    logger.info("%s [%s] %s", metadata.recipeName, metadata.recipeInstanceId, event.name)
   })
 }
