4ac9b546fcff07d46d5651b72c87c0116b879f9b
Francisco Aramburo <francoara@pm.me>
2020-01-29 23:43:33 +0100

    Cleaned old akka protocols

--- baas-protocol-interaction-scheduling/src/main/scala/com/ing/baker/baas/akka/InteractionSchedulingProtocolsSerializer.scala
+++ baas-protocol-interaction-scheduling/src/main/scala/com/ing/baker/baas/akka/InteractionSchedulingProtocolsSerializer.scala
@@ -25,19 +25,7 @@ object InteractionSchedulingProtocolsSerializer {
       forType[com.ing.baker.baas.protocol.ProtocolInteractionExecution.NoInstanceFound.type]
         .register("com.ing.baker.baas.protocol.ProtocolInteractionExecution.NoInstanceFound"),
       forType[com.ing.baker.baas.protocol.ProtocolInteractionExecution.InvalidExecution]
-        .register("com.ing.baker.baas.protocol.ProtocolInteractionExecution.InvalidExecution"),
-      forType[com.ing.baker.baas.protocol.ProtocolPushPullMatching.Push]
-        .register("com.ing.baker.baas.protocol.ProtocolPushPullMatching.Push"),
-      forType[com.ing.baker.baas.protocol.ProtocolPushPullMatching.Pull]
-        .register("com.ing.baker.baas.protocol.ProtocolPushPullMatching.Pull"),
-      forType[com.ing.baker.baas.protocol.ProtocolPushPullMatching.AvailableQuest]
-        .register("com.ing.baker.baas.protocol.ProtocolPushPullMatching.AvailableQuest"),
-      forType[com.ing.baker.baas.protocol.ProtocolQuestCommit.Considering]
-        .register("com.ing.baker.baas.protocol.ProtocolQuestCommit.Considering"),
-      forType[com.ing.baker.baas.protocol.ProtocolQuestCommit.Commit]
-        .register("com.ing.baker.baas.protocol.ProtocolQuestCommit.Commit"),
-      forType[com.ing.baker.baas.protocol.ProtocolQuestCommit.QuestTaken.type]
-        .register("com.ing.baker.baas.protocol.ProtocolQuestCommit.QuestTaken")
+        .register("com.ing.baker.baas.protocol.ProtocolInteractionExecution.InvalidExecution")
     )
   }
 }
