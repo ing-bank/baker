e634bba9ab33bac3d7947739f0e5c86e5c27e6dd
Francisco Aramburo <francoara@pm.me>
2020-03-17 12:38:03 +0100

    WIP fun fun fun

--- baas-smoke-tests/src/test/scala/com/ing/baker/baas/smoke/BakeryFunSpec.scala
+++ baas-smoke-tests/src/test/scala/com/ing/baker/baas/smoke/BakeryFunSpec.scala
@@ -5,6 +5,7 @@ import java.util.UUID
 import cats.effect.{ContextShift, IO, Resource, Timer}
 import cats.implicits._
 import org.http4s.Uri
+import org.http4s.client.Client
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.scalactic.source
 import org.scalatest.compatible.Assertion
@@ -22,14 +23,6 @@ abstract class BakeryFunSpec extends fixture.AsyncFunSpecLike {
     IO.timer(executionContext)
 
   val isWindows: Boolean = sys.props.get("os.name").exists(_.toLowerCase().contains("windows"))
-
-  def getPathSafe(resourcePath: String): String = {
-    val safePath = getClass.getResource(resourcePath).getPath
-    if (isWindows)
-      safePath.tail
-    else
-      safePath
-  }
 
   def eventually[A](f: IO[A]): IO[A] =
     within(20.seconds, 20)(f)
@@ -93,111 +86,13 @@ abstract class BakeryFunSpec extends fixture.AsyncFunSpecLike {
 
     it(specText, testTags: _*) { args =>
 
-      def setup[A](f: IO[A]): IO[Option[A]] =
-        if (args.skipSetup) IO.pure(None) else f.map(Some(_))
 
-      def cleanup[A](f: IO[A]): IO[Unit] =
-        if (args.skipCleanup) IO.unit else f.void
-
-      def processLogger(prefix: String): ProcessLogger = ProcessLogger(
-        line => println(prefix + " " + line),
-        err => stderr.println(Console.RED + err + Console.RESET))
-
-      def exec(prefix: String, command: String): IO[Int] =
-        IO(command.!(processLogger(prefix)))
-
-      val kubernetesAvailable: IO[Boolean] =
-        IO("kubectl --help".!(ProcessLogger(_ => ())) == 0)
-
-      def deleteEnvironment(namespace: String): IO[Unit] = {
-        val prefix = s"[${Console.CYAN}cleaning env $namespace${Console.RESET}]"
-        exec(
-          prefix = prefix,
-          command = s"kubectl delete -f ${getPathSafe("/kubernetes")} -n $namespace"
-        ) *> exec(
-          prefix = prefix,
-          command = s"kubectl delete namespace $namespace"
-        ).void
-      }
-
-
-      val createEnvironment: IO[String] = {
-        for {
-
-          testUUID <- IO(UUID.randomUUID().toString)
-          kubernetesConfigPath = getPathSafe("/kubernetes")
-
-          prefix = s"[${Console.GREEN}creating env $testUUID${Console.RESET}]"
-          _ <- exec(prefix, command = s"kubectl create namespace $testUUID")
-          _ = if (args.skipCleanup) {
-            println(Console.YELLOW + s"### Will skip cleanup after the test, to manually clean the environment run: " + Console.RESET)
-            println(s"\n\tkubectl delete -f $kubernetesConfigPath -n $testUUID && kubectl delete namespace $testUUID\n")
-          }
-        } yield testUUID
-      }
-
-      def applyFile(name: String, namespace: String): IO[Unit] = {
-        val kubernetesConfigPath = getPathSafe("/kubernetes")
-        val prefix = s"[${Console.GREEN}applying file $name $namespace${Console.RESET}]"
-        exec(prefix, command = s"kubectl apply -f $kubernetesConfigPath/$name -n $namespace").void
-
-      }
-
-      case class DefinitionFile(path: String,
-                                namespace: Option[String])
-      object DefinitionFile {
-        def resource(path: String, namespace: String): Resource[IO, DefinitionFile] = {
-          Resource.make(applyFileResource(path, Some(namespace)))(deleteFileResource)
-        }
-
-        def resource(path: String): Resource[IO, DefinitionFile] = {
-          Resource.make(applyFileResource(path, None))(deleteFileResource)
-        }
-
-        private def applyFileResource(path: String, namespace: Option[String]): IO[DefinitionFile] = {
-          val kubernetesConfigPath = getPathSafe("/kubernetes")
-          val prefix = s"[${Console.GREEN}applying file $path $namespace${Console.RESET}]"
-          exec(prefix, command = s"kubectl apply -f $kubernetesConfigPath/$path ${namespace.fold("")(ns => "-n " + ns)}")
-            .map(_ => DefinitionFile(path, namespace))
-        }
-
-        private def deleteFileResource(definitionFile: DefinitionFile): IO[Unit] = {
-          val kubernetesConfigPath = getPathSafe("/kubernetes")
-          val prefix = s"[${Console.CYAN}deleting file ${definitionFile.path} ${definitionFile.namespace}${Console.RESET}]"
-          exec(prefix, command = s"kubectl delete -f $kubernetesConfigPath/${definitionFile.path} ${definitionFile.namespace.fold("")(ns => "-n " + ns)}").void
-        }
-      }
-
-
-      def getPods(namespaceOpt: Option[String]): IO[Int] =
-        namespaceOpt match {
-          case Some(namespace) =>
-            exec(
-              prefix = s"[${Console.GREEN}pods${Console.RESET}]",
-              command = s"kubectl get pods -n $namespace")
-          case None =>
-            IO(println(Console.GREEN + "Skipped startup, will run the tests immediately" + Console.RESET)) *> IO.pure(0)
-        }
-
-
-      val setupWaitTime = 1.minute
-      val setupWaitSplit = 60
-
-      val masterResource = for {
-        namespace <- Resource.make(createEnvironment)(deleteEnvironment)
-        _ <- DefinitionFile.resource("crd-recipe.yaml")
-        client <- BlazeClientBuilder[IO](executionContext).resource
-      } yield client
-
-
+      /*
       def dontSkipTest: IO[Assertion] =
 
         for {
           namespace <- setup(createEnvironment)
           outcome <- BlazeClientBuilder[IO](executionContext).resource.use { client =>
-            val exampleAppClient = new ExampleAppClient(client, args.clientAppHostname)
-            val recipeEventsClient = new EventListenerClient(client, args.eventListenerHostname)
-            val bakerEventsClient = new EventListenerClient(client, args.bakerEventListenerHostname)
 
             for {
               _ <- IO(println(Console.GREEN + s"\nSetting up Bakery environment" + Console.RESET))
@@ -239,6 +134,9 @@ abstract class BakeryFunSpec extends fixture.AsyncFunSpecLike {
         canRun <- kubernetesAvailable
         outcome <- if (canRun) dontSkipTest else skipTest
       } yield outcome).unsafeToFuture()
+      */
+
+      environmentResource.use(runTest(_)).unsafeToFuture()
     }
   }
 }
