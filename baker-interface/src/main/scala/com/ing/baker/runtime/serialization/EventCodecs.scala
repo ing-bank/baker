7ad86af88a0d130af02ca16149d8cf0b93855b3e
yk24na <stas.shevchenko@ing.com>
2020-03-30 14:27:05 +0200

    Add tests for basic structures

--- baker-interface/src/main/scala/com/ing/baker/runtime/serialization/EventCodecs.scala
+++ baker-interface/src/main/scala/com/ing/baker/runtime/serialization/EventCodecs.scala
@@ -1,14 +1,13 @@
 package com.ing.baker.runtime.serialization
 
-import com.ing.baker.il.{CompiledRecipe, EventDescriptor, IngredientDescriptor}
+import com.ing.baker.il.CompiledRecipe
 import com.ing.baker.il.failurestrategy.ExceptionStrategyOutcome
+import com.ing.baker.runtime.common.RejectReason
 import com.ing.baker.runtime.scaladsl.{BakerEvent, EventInstance}
 import com.ing.baker.types
 import io.circe.Encoder._
 import io.circe._
 import io.circe.generic.semiauto._
-import com.ing.baker.runtime.common.RejectReason
-import com.ing.baker.types.Type
 
 object EventCodecs {
 
@@ -24,14 +23,6 @@ object EventCodecs {
         encodeString(value.toString)
     }
 
-  implicit val typeEncoder: Encoder[com.ing.baker.types.Type] = (a: Type) => encodeString(a.getClass.getSimpleName)
-
-  implicit val ingredientDescriptorEncoder: Encoder[IngredientDescriptor] = deriveEncoder[IngredientDescriptor]
-  implicit val eventDescriptorEncoder: Encoder[EventDescriptor] = (a: EventDescriptor) => Json.fromJsonObject(JsonObject(
-    "name" -> encodeString(a.name),
-   "ingredients" -> encodeSeq(ingredientDescriptorEncoder)(a.ingredients)
-  ))
-
   implicit val eventInstanceEncoder: Encoder[EventInstance] = deriveEncoder[EventInstance]
   implicit val rejectReasonEncoder: Encoder[RejectReason] = encodeString.contramap(_.toString)
   implicit val exceptionEncoder: Encoder[ExceptionStrategyOutcome] = deriveEncoder[ExceptionStrategyOutcome]
@@ -40,5 +31,4 @@ object EventCodecs {
     encodeString.contramap[CompiledRecipe](_.getRecipeVisualization.toString)
 
   implicit val bakerEventEncoder: Encoder[BakerEvent] = deriveEncoder[BakerEvent]
-
 }
---
+++ baker-interface/src/test/scala/com/ing/baker/runtime/serialization/BakerEventFormattersSpec.scala
@@ -0,0 +1,43 @@
+package com.ing.baker.runtime.serialization
+
+
+import com.ing.baker.il.failurestrategy.ExceptionStrategyOutcome
+import com.ing.baker.runtime.common.RejectReason
+import com.ing.baker.runtime.scaladsl.EventInstance
+import com.ing.baker.runtime.serialization.BakerEventFormatters._
+import com.ing.baker.types.PrimitiveValue
+import io.circe.syntax._
+import org.scalatest.{FunSpec, Matchers}
+
+class BakerEventFormattersSpec extends FunSpec with Matchers {
+  describe("BakerEventFormatters") {
+    it("should encode RejectReason enum") {
+      RejectReason.values().foreach(
+        value =>
+          value.asJson.noSpaces shouldEqual (s""""${value.toString}"""")
+      )
+    }
+
+    it("should encode EventInstance") {
+      val event = EventInstance("name", Map("id" -> PrimitiveValue(3)))
+      event.asJson.noSpaces shouldEqual """{"name":"name","providedIngredients":{"id":"3"}}"""
+    }
+
+    it("should encode ExceptionStrategyOutcome") {
+      val blockException: ExceptionStrategyOutcome = ExceptionStrategyOutcome.BlockTransition
+      blockException.asJson.noSpaces shouldEqual """{"BlockTransition":{}}"""
+
+      val retryException: ExceptionStrategyOutcome = ExceptionStrategyOutcome.RetryWithDelay(10)
+      retryException.asJson.noSpaces shouldEqual """{"RetryWithDelay":{"delay":10}}"""
+
+      val continueException: ExceptionStrategyOutcome = ExceptionStrategyOutcome.Continue("event name")
+      continueException.asJson.noSpaces shouldEqual """{"Continue":{"eventName":"event name"}}"""
+    }
+
+    it("should encode Throwable") {
+      val exception: Throwable = new RuntimeException("message")
+      println(exception.asJson.noSpaces)
+      exception.asJson.noSpaces shouldEqual """"message""""
+    }
+  }
+}
