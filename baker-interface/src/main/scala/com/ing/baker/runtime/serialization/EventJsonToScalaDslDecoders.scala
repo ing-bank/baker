9e42f5d22f3ed8e7121d6b3824d23e679d89ff55
Timur Evdokimov <info@jacum.com>
2020-03-31 11:20:03 +0200

    dummy decoders

--- baker-interface/src/main/scala/com/ing/baker/runtime/serialization/EventJsonToScalaDslDecoders.scala
+++ baker-interface/src/main/scala/com/ing/baker/runtime/serialization/EventJsonToScalaDslDecoders.scala
@@ -1,17 +1,8 @@
 package com.ing.baker.runtime.serialization
 
-import java.util.Base64
-
-import com.ing.baker.il.CompiledRecipe
-import com.ing.baker.il.failurestrategy.ExceptionStrategyOutcome
 import com.ing.baker.runtime.scaladsl.{BakerEvent, EventInstance, RecipeInstanceCreated}
-import com.ing.baker.types
 import io.circe.Decoder._
 import io.circe._
-import io.circe.generic.semiauto._
-import io.circe.syntax._
-
-import scala.collection.JavaConverters._
 
 object EventJsonToScalaDslDecoders {
 
