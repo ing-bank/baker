3b06d92e7ed8df5e51b2aa2c570d5715f3948f97
Scala Steward <me@scala-steward.org>
2020-03-31 23:36:42 +0200

    Update akka-actor, akka-cluster, ... to 2.5.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 //noinspection TypeAnnotation
 object Dependencies {
 
-  val akkaVersion = "2.5.29"
+  val akkaVersion = "2.5.31"
   
   val http4sVersion = "0.21.2"
   val circeVersion = "0.13.0"
