3e716fa604dbe467cd19b13fe36b9bc839f92449
Francisco Aramburo <francoara@pm.me>
2020-05-20 16:31:02 +0200

    Made creation operations idem on the controllers

--- bakery-controller/src/main/scala/com/ing/bakery/clustercontroller/controllers/InteractionController.scala
+++ bakery-controller/src/main/scala/com/ing/bakery/clustercontroller/controllers/InteractionController.scala
@@ -7,7 +7,6 @@ import com.typesafe.scalalogging.LazyLogging
 import org.http4s.Uri
 import org.http4s.client.Client
 import play.api.libs.json.Format
-import skuber.K8SException
 import skuber.LabelSelector.IsEqualRequirement
 import skuber.api.client.KubernetesClient
 import skuber.apps.v1.{Deployment, ReplicaSet, ReplicaSetList}
@@ -15,7 +14,6 @@ import skuber.json.format._
 import skuber.json.ext.format._
 import skuber.{ConfigMap, Container, LabelSelector, LocalObjectReference, ObjectMeta, Pod, PodList, Protocol, Service, Volume}
 
-import scala.concurrent.Future
 import scala.concurrent.duration._
 
 final class InteractionController(httpClient: Client[IO])(implicit cs: ContextShift[IO], timer: Timer[IO]) extends ControllerOperations[InteractionResource] with LazyLogging {
@@ -24,11 +22,13 @@ final class InteractionController(httpClient: Client[IO])(implicit cs: ContextSh
 
   // TODO make these operations atomic, if one fails we need to rollback previous ones
   def create(resource: InteractionResource, k8s: KubernetesClient): IO[Unit] = {
+    val dpl = deployment(resource)
+    val svc = service(resource)
     for {
-      _ <- attemptOpOrTryOlderVersion(
-        v1 = io(k8s.create[Deployment](deployment(resource))).void,
-        older = io(k8s.create[skuber.ext.Deployment](oldDeployment(resource))).void)
-      deployedService <- io(k8s.create(service(resource)))
+      _ <- idem(attemptOpOrTryOlderVersion(
+        v1 = io(k8s.create[Deployment](dpl)).void,
+        older = io(k8s.create[skuber.ext.Deployment](oldDeployment(resource))).void), s"deployment '${dpl.name}'")
+      deployedService <- applyOrGet(io(k8s.create(svc)), io(k8s.get[Service](svc.name)), s"service '${svc.name}'")
       deployedPort = deployedService.spec
         .flatMap(_.ports.find(_.name == "http-api"))
         .map(_.port)
@@ -38,7 +38,8 @@ final class InteractionController(httpClient: Client[IO])(implicit cs: ContextSh
       interfaces <- Utils.within(10.minutes, split = 300) { // Check every 2 seconds for interfaces
         client.interface.map { interfaces => assert(interfaces.nonEmpty); interfaces }
       }
-      _ <- io(k8s.create(creationContract(resource, address, interfaces)))
+      contract = creationContract(resource, address, interfaces)
+      _ <- idem(io(k8s.create(contract)), s"creation contract config map '${contract.name}'")
       _ = logger.info(s"Deployed interactions from interaction set named '${resource.name}': ${interfaces.map(_.name).mkString(", ")}")
     } yield ()
   }
@@ -65,20 +66,6 @@ final class InteractionController(httpClient: Client[IO])(implicit cs: ContextSh
         v1 = io(k8s.update[Deployment](deployment(resource))).void,
         older = io(k8s.update[skuber.ext.Deployment](oldDeployment(resource))).void)
     } yield ()
-
-  /**
-    * This attempts to make a skuber operation (normaly an api call to api version apps/v1), if the error is 404,
-    * that means we are in an old kubernetes environment and we need to use an older version like extensions/apps/v1beta1
-    */
-  private def attemptOpOrTryOlderVersion(v1: IO[Unit], older: IO[Unit]): IO[Unit] =
-    v1.attempt.flatMap {
-      case Left(e: K8SException) if e.status.code.contains(404) => older
-      case Left(e) => IO.raiseError(e)
-      case Right(a) => IO.pure(a)
-    }
-
-  private def io[A](ref: => Future[A])(implicit cs: ContextShift[IO]): IO[A] =
-    IO.fromFuture(IO(ref))
 
   private def interactionLabel(interaction: InteractionResource): (String, String) = "interaction" -> interaction.name
 
