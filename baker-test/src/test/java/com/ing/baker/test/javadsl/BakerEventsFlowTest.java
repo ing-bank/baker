7348df1dfc91a49d4516ac69d1127237d7a98e99
Sergey Kondratenko <Sergey.Kondratenko@ing.com>
2020-05-29 15:12:07 +0200

    add more unit tests for javadsl

--- baker-test/src/test/java/com/ing/baker/test/javadsl/BakerEventsFlowTest.java
+++ baker-test/src/test/java/com/ing/baker/test/javadsl/BakerEventsFlowTest.java
@@ -13,4 +13,22 @@ public class BakerEventsFlowTest {
     public void testOrderDoesNotMatter() {
         Assert.assertEquals(BakerEventsFlow.of("a", "b", "c"), BakerEventsFlow.of("c", "a", "b"));
     }
+
+    @Test
+    public void testAddEvent() {
+        BakerEventsFlow original = BakerEventsFlow.of("a", "b", "c");
+        Assert.assertNotEquals(original, original.add("d"));
+        Assert.assertEquals(BakerEventsFlow.of("a", "b", "c", "d"), original.add("d"));
+    }
+
+    @Test
+    public void testRemoveEvent() {
+        Assert.assertEquals(BakerEventsFlow.of("a"), BakerEventsFlow.of("a", "Object").removeClass(Object.class));
+    }
+
+    @Test
+    public void testEmpty() {
+        Assert.assertEquals(BakerEventsFlow.of(), BakerEventsFlow.of());
+        Assert.assertEquals(BakerEventsFlow.of(), BakerEventsFlow.of("a").remove("a"));
+    }
 }
