61452b713a6c5e7aebc31d61610d29e878169db5
Tim Linschoten <timlinschoten@outlook.com>
2020-07-13 08:25:58 +0200

    Interaction starter spring (#596)
    * Added baas-node-interaction-spring module
    
    * Added some logging information to the main class of the interaction-spring
    
    * Added cats as a dependency for Bakery interactions

---
+++ baas-node-interaction-spring/src/main/scala/com/ing/baker/baas/interaction/spring/Main.scala
@@ -0,0 +1,52 @@
+package com.ing.baker.baas.interaction.spring
+
+import java.util
+
+import com.ing.baker.baas.interaction.RemoteInteractionLoader
+import com.ing.baker.recipe.javadsl.Interaction
+import com.ing.baker.runtime.scaladsl.InteractionInstance
+import com.typesafe.scalalogging.LazyLogging
+import kamon.Kamon
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
+import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+ * Expects single argument containing Spring configuration
+ */
+object Main extends App with LazyLogging{
+
+  Kamon.init()
+
+  def getImplementations(configurationClassString: String) : List[InteractionInstance] = {
+    val configClass = Class.forName(configurationClassString)
+    logger.info("Class found: " + configClass)
+    val ctx = new AnnotationConfigApplicationContext();
+    logger.info("Context created")
+    ctx.register(configClass)
+    logger.info("Context registered")
+    ctx.refresh()
+    logger.info("Context refreshed")
+    val interactions: util.Map[String, Interaction] =
+      ctx.getBeansOfType(classOf[com.ing.baker.recipe.javadsl.Interaction])
+    interactions.asScala.values.map(implementation => {
+      val instance = InteractionInstance.unsafeFrom(implementation)
+      logger.info("Added implementation: " + instance.name)
+      instance
+    }).toList
+  }
+
+  private def runApp(configurationClassString: String): Unit =
+    try {
+      logger.info("Starting for configuration: " + configurationClassString)
+      val implementations = getImplementations(configurationClassString)
+      logger.info("Starting RemoteInteractionLoader")
+      RemoteInteractionLoader.apply(getImplementations(configurationClassString))
+    } catch {
+      case ex: Exception =>
+        throw new IllegalStateException(s"Unable to initialize the interaction instances", ex)
+    }
+
+  args.headOption.map(runApp).getOrElse(throw new IllegalAccessException("Please provide a Spring configuration containing valid interactions"))
+}
--- build.sbt
+++ build.sbt
@@ -295,6 +295,8 @@ lazy val `baas-node-interaction` = project.in(file("baas-node-interaction"))
       http4s,
       http4sDsl,
       http4sServer,
+      catsEffect,
+      catsCore,
       kamon,
       kamonPrometheus
     ) ++ testDeps(
@@ -303,6 +305,28 @@ lazy val `baas-node-interaction` = project.in(file("baas-node-interaction"))
     )
   )
   .dependsOn(`baas-protocol-interaction-scheduling`, `baker-interface`)
+
+lazy val `baas-node-interaction-spring` = project.in(file("baas-node-interaction-spring"))
+  .settings(defaultModuleSettings)
+  .settings(
+    moduleName := "baas-node-interaction-spring",
+    libraryDependencies ++= Seq(
+      slf4jApi,
+      logback,
+      http4s,
+      http4sDsl,
+      http4sServer,
+      kamon,
+      kamonPrometheus,
+      springCore,
+      springContext,
+      scalaLogging
+    ) ++ testDeps(
+      scalaTest,
+      logback
+    )
+  )
+  .dependsOn(`baas-node-interaction`, `recipeDsl`)
 
 lazy val `bakery-controller` = project.in(file("bakery-controller"))
   .settings(defaultModuleSettings)
