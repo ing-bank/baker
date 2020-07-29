157bd707fd05cdac7de25c29123b6b607ad4450a
Scala Steward <me@scala-steward.org>
2020-07-29 02:27:26 +0200

    Update protobuf-java to 3.12.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -83,7 +83,7 @@ object Dependencies {
 
   val scalapbRuntime =            "com.thesamet.scalapb"       %% "scalapb-runtime"                    % scalapbVersion % "protobuf"
 
-  val protobufJava =              "com.google.protobuf"        % "protobuf-java"                       % "3.12.2"
+  val protobufJava =              "com.google.protobuf"        % "protobuf-java"                       % "3.12.4"
 
   val betterFiles =               "com.github.pathikrit"       %% "better-files"                       % "3.9.1"
 
