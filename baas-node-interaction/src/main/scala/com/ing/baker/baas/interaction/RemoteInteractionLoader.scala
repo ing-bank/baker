eace64314470ae4d4bfa5c7ee95d744e74afd98b
Francisco Aramburo <francoara@pm.me>
2020-07-30 11:36:14 +0200

    extra tests

--- baas-node-interaction/src/main/scala/com/ing/baker/baas/interaction/RemoteInteractionLoader.scala
+++ baas-node-interaction/src/main/scala/com/ing/baker/baas/interaction/RemoteInteractionLoader.scala
@@ -19,6 +19,7 @@ object RemoteInteractionLoader {
     val keystoreType = config.getString("baas-component.interaction.https-keystore-type")
 
     val address = InetSocketAddress.createUnresolved("0.0.0.0", port)
+    val healthServiceAddress = InetSocketAddress.createUnresolved("0.0.0.0", 9999)
     val tlsConfig =
       if(httpsEnabled) Some(BakeryHttp.TLSConfig(password = keystorePassword, keystorePath = keystorePath, keystoreType = keystoreType))
       else None
@@ -28,7 +29,7 @@ object RemoteInteractionLoader {
     implicit val timer: Timer[IO] = IO.timer(executionContext)
 
     RemoteInteractionService.resource(implementations, address, tlsConfig)
-      .flatMap(_ => HealthService.resource)
+      .flatMap(_ => HealthService.resource(healthServiceAddress))
       .use(_ => IO.never)
       .unsafeRunAsyncAndForget()
   }
