198fe311708f243b285744e61b73a520f42b3c7d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-16 09:00:58 +0200

    Update spring-context, spring-core to 5.2.9.RELEASE (#662)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -124,8 +124,8 @@ object Dependencies {
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 
 
-  val springContext = "org.springframework" % "spring-context" % "5.2.8.RELEASE"
-  val springCore = "org.springframework" % "spring-core" % "5.2.8.RELEASE"
+  val springContext = "org.springframework" % "spring-context" % "5.2.9.RELEASE"
+  val springCore = "org.springframework" % "spring-core" % "5.2.9.RELEASE"
 
   def scopeDeps(scope: String, modules: Seq[ModuleID]) = modules.map(m => m % scope)
 
