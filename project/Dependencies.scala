8982e68e2eeeedc936fbc1db97ff06a4447aef78
Scala Steward <me@scala-steward.org>
2020-11-19 14:48:31 +0100

    Update metrics-jmx to 4.0.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -72,7 +72,7 @@ object Dependencies {
   val cassandraUnit = "org.cassandraunit" % "cassandra-unit" % "4.3.1.0"
   val cassandraDriverCore = "com.datastax.oss" % "java-driver-core" % "4.9.0"
   val cassandraDriverQueryBuilder = "com.datastax.oss" % "java-driver-query-builder" % "4.9.0"
-  val cassandraDriverMetrics = "io.dropwizard.metrics" % "metrics-jmx" % "4.0.5"
+  val cassandraDriverMetrics = "io.dropwizard.metrics" % "metrics-jmx" % "4.0.7"
 
   val skuber = "io.skuber" %% "skuber" % "2.6.0"
   val play = "com.typesafe.play" %% "play-json" % "2.9.1"
