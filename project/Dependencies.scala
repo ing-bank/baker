11f24956818f92319ca81f9b894f252e44244b63
Scala Steward <me@scala-steward.org>
2020-11-26 08:07:34 +0100

    Update cats-core to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -89,7 +89,7 @@ object Dependencies {
   val circeGenericExtras = "io.circe" %% "circe-generic-extras" % circeVersion
 
   val catsEffect = "org.typelevel" %% "cats-effect" % "2.2.0"
-  val catsCore = "org.typelevel" %% "cats-core" % "2.2.0"
+  val catsCore = "org.typelevel" %% "cats-core" % "2.3.0"
   val console4Cats = "dev.profunktor" %% "console4cats" % "0.8.0"
   val catsRetry = "com.github.cb372" %% "cats-retry" % "2.0.0"
 
