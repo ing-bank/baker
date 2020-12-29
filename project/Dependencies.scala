1f176dab854458bbe35e8c708f8c0402c0ee5c2c
Scala Steward <me@scala-steward.org>
2020-12-30 00:33:08 +0100

    Update joda-time to 2.10.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -114,7 +114,7 @@ object Dependencies {
   val typeSafeConfig = "com.typesafe" % "config" % "1.4.1"
 
   val objenisis = "org.objenesis" % "objenesis" % "3.1"
-  val jodaTime = "joda-time" % "joda-time" % "2.10.8"
+  val jodaTime = "joda-time" % "joda-time" % "2.10.9"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.30"
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   val logstash =  "net.logstash.logback" % "logstash-logback-encoder" % "6.4"
