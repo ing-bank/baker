ba64640ff7eb1b8dfb89b081e2c0629a748259dc
yk24na <stas.shevchenko@ing.com>
2021-02-03 17:48:01 +0100

    Removed unused code

--- bakery/baker-client/src/test/scala/com/ing/bakery/common/FailoverUtilsSpec.scala
+++ bakery/baker-client/src/test/scala/com/ing/bakery/common/FailoverUtilsSpec.scala
@@ -93,39 +93,6 @@ class FailoverUtilsSpec extends AnyFunSpec with MockitoSugar {
       assert(config.initialDelay == 5.milliseconds)
       assert(config.retryTimes == 2)
     }
-
-//    it("404 example") {
-//      import cats.effect._
-//      import org.http4s._
-//      import org.http4s.dsl.io._
-//      import org.http4s.implicits._
-//      import org.http4s.server.blaze._
-//
-//      import scala.concurrent.ExecutionContext.global
-//      implicit val cs: ContextShift[IO] = IO.contextShift(global)
-//      implicit val timer: Timer[IO] = IO.timer(global)
-//
-//      val app = HttpRoutes.of[IO] {
-//        case org.http4s.dsl.io.GET -> org.http4s.dsl.io.Root / "hello" / name =>
-//          Ok(s"Hello, $name.")
-//      }.orNotFound
-//
-//
-//      val server = BlazeServerBuilder[IO](global).bindHttp(8085, "localhost").withHttpApp(app).resource
-//
-//      val fiber = server.use(_ => IO.never).start.unsafeRunSync()
-//
-//      import org.http4s.client.blaze._
-//      import org.http4s.client._
-//
-//
-//      val service: ExecutorService = Executors.newFixedThreadPool(5)
-//      val httpClient: Client[IO] = JavaNetClientBuilder[IO](Blocker.liftExecutorService(service)).create
-//
-//      val helloJames = httpClient.expect[String]("http://localhost:8085/James")
-//      println(helloJames.unsafeRunSync())
-//
-//    }
   }
 
   private def testMethod(fos: FailoverState, func: Uri => IO[Request[IO]]): BakerResult = {
