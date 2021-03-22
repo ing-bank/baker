b8671f00517d2d8d575fa0a29dff2cf48945665c
Scala Steward <me@scala-steward.org>
2021-03-22 06:12:39 +0100

    Update objenesis to 3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -112,7 +112,7 @@ object Dependencies {
 
   val typeSafeConfig = "com.typesafe" % "config" % "1.4.1"
 
-  val objenisis = "org.objenesis" % "objenesis" % "3.1"
+  val objenisis = "org.objenesis" % "objenesis" % "3.2"
   val jodaTime = "joda-time" % "joda-time" % "2.10.10"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.30"
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
