4ccb0f92f2e13a5253c62dfbab157e336d644912
Scala Steward <me@scala-steward.org>
2021-03-29 12:22:46 +0200

    Update mockito-scala, ... to 1.16.33

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val http4sVersion = "0.21.20"
   val fs2Version = "2.5.3"
   val circeVersion = "0.13.0"
-  val mockitoScalaVersion = "1.16.29"
+  val mockitoScalaVersion = "1.16.33"
   val catsVersion = "2.3.3"
   val catsCoreVersion = "2.4.2"
   val scalapbVersion = scalapb.compiler.Version.scalapbVersion
