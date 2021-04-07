56f72e4dd03b3a1691ef6c2a3ed796fdfc119498
Scala Steward <me@scala-steward.org>
2021-04-07 06:37:15 +0200

    Update http4s-blaze-client, ... to 0.21.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val akkaManagementVersion = "1.0.10"
   val akkaPersistenceCassandraVersion = "1.0.5"
   val akkaHttpVersion = "10.2.4"
-  val http4sVersion = "0.21.21"
+  val http4sVersion = "0.21.22"
   val fs2Version = "2.5.3"
   val circeVersion = "0.13.0"
   val mockitoScalaVersion = "1.16.33"
