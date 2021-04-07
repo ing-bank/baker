915ec3edd1cd16c99a90ffc9c5c06103dec08e71
Scala Steward <me@scala-steward.org>
2021-04-07 20:29:36 +0200

    Update akka-actor, akka-cluster, ... to 2.6.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 //noinspection TypeAnnotation
 object Dependencies {
 
-  val akkaVersion = "2.6.13"
+  val akkaVersion = "2.6.14"
   val akkaManagementVersion = "1.0.10"
   val akkaPersistenceCassandraVersion = "1.0.5"
   val akkaHttpVersion = "10.2.4"
