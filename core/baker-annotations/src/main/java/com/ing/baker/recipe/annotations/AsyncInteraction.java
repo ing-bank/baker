cd251556ca1d24adca369d5567764eecfdd5ebd8
XK00LJ <tim.linschoten@ing.nl>
2021-05-18 10:45:51 +0200

    Fix javadoc problems.

--- core/baker-annotations/src/main/java/com/ing/baker/recipe/annotations/AsyncInteraction.java
+++ core/baker-annotations/src/main/java/com/ing/baker/recipe/annotations/AsyncInteraction.java
@@ -3,6 +3,9 @@ package com.ing.baker.recipe.annotations;
 import javax.inject.Qualifier;
 import java.lang.annotation.*;
 
+/**
+ * Annotation for an interaction to show it supports Async calls
+ */
 @Qualifier
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
