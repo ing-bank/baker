cd39a78c6105f5e2e58d7e11f2ad289323c0b776
XK00LJ <tim.linschoten@ing.nl>
2021-05-20 13:12:26 +0200

    Added a test for the NoSuchProcessException to ensure this was not broken in the Baker client.

--- bakery/client/src/test/scala/com/ing/bakery/common/FailoverUtilsSpec.scala
+++ bakery/client/src/test/scala/com/ing/bakery/common/FailoverUtilsSpec.scala
@@ -18,11 +18,17 @@ import org.scalatest.{Assertion, FutureOutcome}
 import io.circe.generic.auto._
 import com.ing.baker.runtime.serialization.JsonEncoders._
 import org.http4s.circe.jsonEncoderOf
-
 import java.net.InetSocketAddress
+
+import com.ing.baker.runtime.common.BakerException
+import com.ing.baker.runtime.common.BakerException.NoSuchProcessException
+import com.ing.baker.runtime.serialization.JsonDecoders.bakerResultDecoder
+import com.ing.bakery.common.FailoverUtils.callWithFailOver
+
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success, Try}
 
 
 class FailoverUtilsSpec extends FixtureAsyncFunSpec   {
@@ -41,6 +47,7 @@ class FailoverUtilsSpec extends FixtureAsyncFunSpec   {
   case class Context(client: Client[IO],
                      server200: Uri,
                      server400: Uri,
+                     server404: Uri,
                      server500: Uri)
 
   def contextBuilder: Resource[IO, Context] = {
@@ -73,6 +80,17 @@ class FailoverUtilsSpec extends FixtureAsyncFunSpec   {
         )
       .resource
 
+      server404 <- BlazeServerBuilder[IO](ExecutionContext.global)
+        .bindSocketAddress( InetSocketAddress.createUnresolved("localhost", 0))
+        .withHttpApp(
+          Router("/" -> HttpRoutes.of[IO] {
+            case _ =>
+              callbackCollector.append("404")
+              NotFound()
+          }).orNotFound
+        )
+        .resource
+
       server400 <- BlazeServerBuilder[IO](ExecutionContext.global)
         .bindSocketAddress( InetSocketAddress.createUnresolved("localhost", 0))
         .withHttpApp(
@@ -88,6 +106,7 @@ class FailoverUtilsSpec extends FixtureAsyncFunSpec   {
     } yield Context(client,
       Uri.unsafeFromString(s"http://localhost:${server200.address.getPort}"),
       Uri.unsafeFromString(s"http://localhost:${server400.address.getPort}"),
+      Uri.unsafeFromString(s"http://localhost:${server404.address.getPort}"),
       Uri.unsafeFromString(s"http://localhost:${server500.address.getPort}")
     )
   }
@@ -104,7 +123,7 @@ class FailoverUtilsSpec extends FixtureAsyncFunSpec   {
       val fos = new FailoverState(EndpointConfig(IndexedSeq(context.server500, context.server200)))
       for {
         result <- callWithFailOver(fos, context.client,
-        (uri, _) => GET(uri / "app" / "recipes"), Seq.empty,
+          (uri, _) => GET(uri / "app" / "recipes"), Seq.empty,
           FailoverUtils.handleHttpErrors, None)(executionContext, bakerResultDecoder)
       } yield {
         assert(result == BakerResult(List.empty[Int]))
@@ -134,6 +153,33 @@ class FailoverUtilsSpec extends FixtureAsyncFunSpec   {
       } yield {
         assert(result == BakerResult(List.empty[Int]))
         assert(callbackCollector.toList == List("500", "400", "200"))
+      }
+    }
+
+    test ("Return correct NoSuchProcessException") { context =>
+      callbackCollector.clear()
+      val fos = new FailoverState(EndpointConfig(IndexedSeq(context.server404)))
+
+      for {
+        either <- callWithFailOver(fos, context.client,
+          (uri, _) => GET(uri / "app" / "recipes"), Seq.empty,
+          FailoverUtils.handleFallbackAwareErrors, None)(executionContext, bakerResultDecoder).attempt
+      } yield {
+        assert(either.left.get.isInstanceOf[NoSuchProcessException])
+      }
+    }
+
+    test ("Return correct NoSuchProcessException after retry") { context =>
+      callbackCollector.clear()
+      val fos = new FailoverState(EndpointConfig(IndexedSeq(context.server404)))
+
+      for {
+        either <- callWithFailOver(fos, context.client,
+          (uri, _) => GET(uri / "app" / "recipes"), Seq.empty,
+          FailoverUtils.handleFallbackAwareErrors,
+            Some(EndpointConfig(IndexedSeq(context.server404, context.server404))))(executionContext, bakerResultDecoder).attempt
+      } yield {
+        assert(either.left.get.isInstanceOf[NoSuchProcessException])
       }
     }
   }
