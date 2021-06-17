b6242db976f0f722b07c85d2192ad9cf22c4526d
Tim Linschoten <timlinschoten@outlook.com>
2021-06-17 14:21:48 +0200

    More restful dashboard (#962)
    * Added NotFound page.
    Changed the recipe page to now have a more restfull approach.
    
    * Changed the recipe instances to also be more REST based.
    
    * Changed the recipe instances to also be more REST based.

--- bakery/dashboard/src/app/app-routing.module.ts
+++ bakery/dashboard/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RecipesComponent } from './recipes/recipes.component';
+import {RecipesComponent} from './recipes/recipes.component';
 import {InteractionsComponent} from "./interactions/interactions.component";
 import {InstancesComponent} from "./instances/instances.component";
 import {HomeComponent} from "./home/home.component";
+import {NotFoundComponent} from "./notfound/notfound.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes/:recipeId', component: RecipesComponent },
   { path: 'interactions', component: InteractionsComponent },
   { path: 'instances', component: InstancesComponent },
-
+  { path: 'instances/:recipeInstanceId', component: InstancesComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
