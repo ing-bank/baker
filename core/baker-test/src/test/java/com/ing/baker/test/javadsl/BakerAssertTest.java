69c65f94caf3945899b7a68cad08aa5ec0e38684
Sergey Kondratenko <Sergey.Kondratenko@ing.com>
2021-09-10 12:15:52 +0200

    fix tests

--- core/baker-test/src/test/java/com/ing/baker/test/javadsl/BakerAssertTest.java
+++ core/baker-test/src/test/java/com/ing/baker/test/javadsl/BakerAssertTest.java
@@ -10,40 +10,62 @@ import com.ing.baker.test.javadsl.recipe.OrderPlaced;
 import com.ing.baker.test.javadsl.recipe.ReserveItemsImpl;
 import com.ing.baker.test.javadsl.recipe.WebshopRecipe;
 import com.typesafe.config.ConfigFactory;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class BakerAssertTest {
 
-    private final Baker baker = AkkaBaker.java(ConfigFactory.defaultApplication(), ActorSystem.apply("test"),
+    private static final Baker baker = AkkaBaker.java(ConfigFactory.defaultApplication(), ActorSystem.apply("test"),
             Collections.singletonList(InteractionInstance.from(new ReserveItemsImpl())));
 
-    private BakerAssert createBakerAssert() throws Exception {
-        String recipeInstanceId = UUID.randomUUID().toString();
+    private static final String recipeId;
 
-        String recipeId = baker.addRecipe(RecipeCompiler.compileRecipe(WebshopRecipe.TEST_RECIPE), true).get(1, TimeUnit.SECONDS);
+    static {
+        try {
+            recipeId = baker.addRecipe(RecipeCompiler.compileRecipe(WebshopRecipe.RECIPE), true)
+                    .get(1, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            throw new AssertionError("");
+        }
+    }
+
+    private BakerAssert bakerAssert;
+    private String recipeInstanceId;
+
+    @Before
+    public void init() throws ExecutionException, InterruptedException, TimeoutException {
+        recipeInstanceId = UUID.randomUUID().toString();
 
         baker.bake(recipeId, recipeInstanceId);
 
-        baker.fireEvent(recipeInstanceId, EventInstance.from(new OrderPlaced("order-1",
-                Arrays.asList("item-1", "item-2", "item-3"))));
+        bakerAssert = BakerAssert.of(baker, recipeInstanceId);
+    }
 
-        return BakerAssert.of(baker, recipeInstanceId);
+    private void fireSensoryEvent(String orderId) {
+        baker.fireEvent(recipeInstanceId, EventInstance.from(new OrderPlaced(orderId,
+                Arrays.asList("item-1", "item-2", "item-3"))));
     }
 
     @Test
-    public void test() throws Exception {
-        BakerAssert bakerAssert = createBakerAssert();
-        Assert.assertNotNull(bakerAssert);
-
-        BakerEventsFlow happyFlow = BakerEventsFlow.of("OrderPlaced", "ItemsReserved");
+    public void testHappy() throws Exception {
+        fireSensoryEvent("order-1");
         bakerAssert
-                .waitFor(happyFlow)
-                .assertEventsFlow(happyFlow);
+                .waitFor(WebshopRecipe.HAPPY_FLOW)
+                .assertEventsFlow(WebshopRecipe.HAPPY_FLOW);
+    }
+
+    @Test
+    public void testAnotherHappy() {
+        fireSensoryEvent("order-2");
+        bakerAssert
+                .waitFor(WebshopRecipe.HAPPY_FLOW)
+                .assertEventsFlow(WebshopRecipe.HAPPY_FLOW);
     }
 }
