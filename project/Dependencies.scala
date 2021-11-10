6b68ec061fe8f6dec22e8769273907c4471f8a58
Scala Steward <me@scala-steward.org>
2021-11-10 18:34:41 +0100

    Update java-driver-query-builder to 4.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
 
   val cassandraUnit = "org.cassandraunit" % "cassandra-unit" % "4.3.1.0"
   val cassandraDriverCore = "com.datastax.oss" % "java-driver-core" % "4.12.1"
-  val cassandraDriverQueryBuilder = "com.datastax.oss" % "java-driver-query-builder" % "4.11.3"
+  val cassandraDriverQueryBuilder = "com.datastax.oss" % "java-driver-query-builder" % "4.13.0"
   val cassandraDriverMetrics = "io.dropwizard.metrics" % "metrics-jmx" % "4.2.4"
 
   val skuber = "io.skuber" %% "skuber" % "2.6.2"
