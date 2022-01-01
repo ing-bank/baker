febbd20c0e9ae56d500ceba7208db1bf6f0de525
Scala Steward <me@scala-steward.org>
2022-01-01 03:38:35 +0100

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=v3.0.0

--- bakery/state/src/main/scala/com/ing/bakery/baker/LocalhostInteractions.scala
+++ bakery/state/src/main/scala/com/ing/bakery/baker/LocalhostInteractions.scala
@@ -1,12 +1,13 @@
 package com.ing.bakery.baker
 
 import akka.actor.ActorSystem
-import cats.effect.{ContextShift, IO, Resource, Timer}
+import cats.effect.{IO, Resource}
 import com.ing.baker.runtime.akka.internal.DynamicInteractionManager
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import org.http4s.Uri
 import org.http4s.client.Client
+import cats.effect.Temporal
 
 /**
   * Discovers interactions running on localhost
@@ -17,7 +18,7 @@ class LocalhostInteractions(config: Config,
                             port: Option[Int] = None)
   extends DynamicInteractionManager with RemoteInteractionDiscovery with LazyLogging {
   protected implicit val contextShift: ContextShift[IO] = IO.contextShift(system.dispatcher)
-  protected implicit val timer: Timer[IO] = IO.timer(system.dispatcher)
+  protected implicit val timer: Temporal[IO] = IO.timer(system.dispatcher)
   protected def apiUrlPrefix: String = config.getString("baker.interactions.localhost.api-url-prefix")
   protected def localhostPort: Int = port.getOrElse(config.getInt("baker.interactions.localhost.port"))
 
