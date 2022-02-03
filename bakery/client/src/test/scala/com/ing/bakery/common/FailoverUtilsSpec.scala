968a54da55905b458d090da9050a540ff41196f6
Scala Steward <me@scala-steward.org>
2022-02-03 19:04:55 +0100

    Applied Scalafix rule(s) github:http4s/http4s/v0_22?sha=series/0.22

--- bakery/client/src/test/scala/com/ing/bakery/common/FailoverUtilsSpec.scala
+++ bakery/client/src/test/scala/com/ing/bakery/common/FailoverUtilsSpec.scala
@@ -7,12 +7,12 @@ import com.ing.bakery.scaladsl.{BakerClient, EndpointConfig}
 import org.http4s.Method.GET
 import org.http4s._
 import org.http4s.client.Client
-import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.blaze.client.BlazeClientBuilder
 import org.http4s.client.dsl.io._
 import org.http4s.dsl.io._
 import org.http4s.implicits._
 import org.http4s.server.Router
-import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.blaze.server.BlazeServerBuilder
 import org.scalatest.funspec.FixtureAsyncFunSpec
 import org.scalatest.{Assertion, FutureOutcome}
 import io.circe.generic.auto._
