5561dd1e28667e40cac205c6ae0a8ef12d349171
Scala Steward <me@scala-steward.org>
2022-04-04 01:41:07 +0200

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=v3.0.0

--- bakery/state/src/main/scala/com/ing/bakery/baker/RecipeLoader.scala
+++ bakery/state/src/main/scala/com/ing/bakery/baker/RecipeLoader.scala
@@ -4,7 +4,7 @@ import java.io.{ByteArrayInputStream, File, InputStream}
 import java.nio.file.{Files, Path}
 import java.util.Base64
 import java.util.zip.{GZIPInputStream, ZipException}
-import cats.effect.{ContextShift, IO, Timer}
+import cats.effect.IO
 import cats.implicits._
 import com.ing.baker.il.CompiledRecipe
 import com.ing.baker.runtime.akka.actor.protobuf
@@ -17,16 +17,17 @@ import scala.util.Try
 import java.nio.file.Files
 import java.nio.file.attribute.BasicFileAttributes
 import scala.concurrent.duration.FiniteDuration
+import cats.effect.Temporal
 
 object RecipeLoader extends LazyLogging {
 
   def pollRecipesUpdates(path: String, bakery: Bakery, duration: FiniteDuration)
-                        (implicit timer: Timer[IO], cs: ContextShift[IO]): IO[Unit] = {
+                        (implicit timer: Temporal[IO]): IO[Unit] = {
     def pollRecipes: IO[Unit] = loadRecipesIntoBaker(path, bakery.baker) >> IO.sleep(duration) >> IO.defer(pollRecipes)
     pollRecipes
   }
 
-  def loadRecipesIntoBaker(path: String, baker: Baker)(implicit cs: ContextShift[IO]): IO[Unit] =
+  def loadRecipesIntoBaker(path: String, baker: Baker): IO[Unit] =
     for {
       recipes <- RecipeLoader.loadRecipes(path)
       _ <-  recipes.traverse { record =>
