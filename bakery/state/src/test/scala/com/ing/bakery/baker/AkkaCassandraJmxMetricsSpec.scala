9bc1c8bbdb257b1c68e7947c1c47afdb7fd568e7
Wessel W. Bakker <wwbakker@gmail.com>
2022-05-18 11:06:37 +0200

    - Skip test when running on apple silicon because it is not (yet) supported. (#1236)

--- bakery/state/src/test/scala/com/ing/bakery/baker/AkkaCassandraJmxMetricsSpec.scala
+++ bakery/state/src/test/scala/com/ing/bakery/baker/AkkaCassandraJmxMetricsSpec.scala
@@ -1,10 +1,7 @@
 package com.ing.bakery.baker
 
 
-import java.io.CharArrayWriter
-import java.net.InetSocketAddress
-import java.util.UUID
-import akka.actor.{ActorLogging, ActorSystem, NoSerializationVerificationNeeded, Props}
+import akka.actor.{ActorLogging, ActorRef, ActorSystem, NoSerializationVerificationNeeded, Props}
 import akka.pattern.ask
 import akka.persistence.PersistentActor
 import akka.sensors.actor.PersistentActorMetrics
@@ -21,6 +18,9 @@ import org.scalatest.concurrent.Eventually
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.time.{Millis, Seconds, Span}
 
+import java.io.CharArrayWriter
+import java.net.InetSocketAddress
+import java.util.UUID
 import scala.Console.println
 import scala.collection.JavaConverters._
 import scala.collection.convert.ImplicitConversions._
@@ -33,20 +33,29 @@ class AkkaCassandraJmxMetricsSpec extends AnyFreeSpec with LazyLogging with Even
   implicit override val patienceConfig: PatienceConfig =
     PatienceConfig(timeout = scaled(Span(2, Seconds)), interval = scaled(Span(5, Millis)))
 
-  val cassandra: Unit = {
-    try {
-      startEmbeddedCassandra("cassandra-server.yaml")
-    } catch {
-      case e: Exception =>
+  implicit val ec: ExecutionContext = ExecutionContext.global
+  private var system: Option[ActorSystem] = None
+  private var persistenceInitActor : Option[ActorRef] = None
+
+  // Currently apple silicon architecture is not supported (and no workarounds exist). Skip this test in that case.
+  // more info https://stackoverflow.com/questions/68315912/how-to-start-cassandra-on-a-m1-macbook
+  // TODO: fix when a fix is available.
+  val runningOnAppleSilicon: Boolean = System.getProperty("os.arch") == "aarch64"
+
+  override protected def beforeAll(): Unit = {
+    if (!runningOnAppleSilicon) {
+      try {
+        startEmbeddedCassandra("cassandra-server.yaml")
+      } catch {
+        case _: Exception =>
+      }
+      system = Some(ActorSystem("baker"))
+      persistenceInitActor = system.map(_.actorOf(Props(classOf[AwaitPersistenceInit]), s"persistenceInit-${UUID.randomUUID().toString}"))
     }
   }
-  implicit val ec: ExecutionContext = ExecutionContext.global
-  private val system: ActorSystem = ActorSystem("baker")
-  private val persistenceInitActor = system.actorOf(Props(classOf[AwaitPersistenceInit]), s"persistenceInit-${UUID.randomUUID().toString}")
-
 
   override protected def afterAll(): Unit = {
-    system.terminate()
+    system.map(_.terminate())
   }
 
   def createSession: CqlSession = {
@@ -59,50 +68,54 @@ class AkkaCassandraJmxMetricsSpec extends AnyFreeSpec with LazyLogging with Even
   "Launch cassandra and akka app, and ensure it works" - {
 
     "starts actor system and pings the bootstrap actor" in {
-      pingActor
+      if (!runningOnAppleSilicon) {
+        pingActor
+      }
     }
 
     "after some activity exports some jmx metrics" in {
-      val session = createSession
-      session.execute("create table akka.test (id int, a text, b text, primary key(id))")
-      for (i <- 1 to 100) {
-        session
-          .execute(
-            insertInto("akka", "test")
-              .values(Map[String, Term](
-                "id" -> literal(i),
-                "a" -> literal(s"A${i}"),
-                "b" -> literal(s"B$i")).asJava).build()
-          )
-      }
+      if (!runningOnAppleSilicon) {
+        val session = createSession
+        session.execute("create table akka.test (id int, a text, b text, primary key(id))")
+        for (i <- 1 to 100) {
+          session
+            .execute(
+              insertInto("akka", "test")
+                .values(Map[String, Term](
+                  "id" -> literal(i),
+                  "a" -> literal(s"A${i}"),
+                  "b" -> literal(s"B$i")).asJava).build()
+            )
+        }
 
-      import java.lang.management.ManagementFactory
-      val mbs = ManagementFactory.getPlatformMBeanServer
-      mbs.queryMBeans(null, null).map(_.getObjectName).filter(_.toString.startsWith("akka")).foreach(println)
-      val datastaxMbeans = mbs.queryMBeans(null, null).filter(_.getObjectName.getDomain == "com.datastax.oss.driver")
-//      datastaxMbeans.foreach(println)
-      assert(datastaxMbeans.nonEmpty)
+        import java.lang.management.ManagementFactory
+        val mbs = ManagementFactory.getPlatformMBeanServer
+        mbs.queryMBeans(null, null).map(_.getObjectName).filter(_.toString.startsWith("akka")).foreach(println)
+        val datastaxMbeans = mbs.queryMBeans(null, null).filter(_.getObjectName.getDomain == "com.datastax.oss.driver")
+        //      datastaxMbeans.foreach(println)
+        assert(datastaxMbeans.nonEmpty)
+      }
     }
 
     "ensure prometheus JMX scraping is working" in {
+      if (!runningOnAppleSilicon) {
+        for (i <- 1 to 100000) {
+          pingActor
+        }
+        val prometheusRegistry = MetricService.registry
 
-      for (i <- 1 to 100000) {
-        pingActor
+        val writer = new CharArrayWriter(16 * 1024)
+        TextFormat.write004(writer, prometheusRegistry.metricFamilySamples)
+        val content = writer.toString
+        println(content)
+        assert(content.split("\n").exists(_.startsWith("cassandra_cql")))
       }
-      val prometheusRegistry = MetricService.registry
-
-      val writer = new CharArrayWriter(16 * 1024)
-      TextFormat.write004(writer, prometheusRegistry.metricFamilySamples)
-      val content = writer.toString
-      println(content)
-      assert(content.split("\n").exists(_.startsWith("cassandra_cql")))
     }
-
   }
 
 
   private def pingActor = {
-    val r = Await.result(persistenceInitActor.ask(Ping)(Timeout.durationToTimeout(30 seconds)), 40 seconds)
+    val r = Await.result(persistenceInitActor.get.ask(Ping)(Timeout.durationToTimeout(30 seconds)), 40 seconds)
     assert(r.toString == "Pong")
   }
 }
