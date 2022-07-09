a242c92c5a670f3f12d992ff2eb4abe192cac242
Scala Steward <scala_steward@virtuslab.com>
2022-07-09 20:32:31 +0000

    Update sensors-core to 0.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -76,7 +76,7 @@ object Dependencies {
 
   val prometheus = "io.prometheus" % "simpleclient_hotspot" % "0.15.0"
   val prometheusJmx = "io.prometheus.jmx" % "collector" % "0.17.0"
-  val sensors =  "nl.pragmasoft.sensors" %% "sensors-core" % "0.2.5"
+  val sensors =  "nl.pragmasoft.sensors" %% "sensors-core" % "0.3.0"
 
   val cassandraUnit = "org.cassandraunit" % "cassandra-unit" % "4.3.1.0"
   val cassandraDriverCore = "com.datastax.oss" % "java-driver-core" % "4.14.1"
