14a8a0be30fe6a24128691eb6c081a5a075ed05c
XK00LJ <tim.linschoten@ing.nl>
2022-11-09 10:54:18 +0100

    Got the prefix from the path.

--- http/baker-http-dashboard/src/app/app-routing.module.ts
+++ http/baker-http-dashboard/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {InstancesComponent} from "./instances/instances.component";
 import {InteractionsComponent} from "./interactions/interactions.component";
 import {NgModule} from "@angular/core";
 import {NotFoundComponent} from "./notfound/notfound.component";
+import {AppSettingsService} from "./app.settings";
 
 import {RecipesComponent} from "./recipes/recipes.component";
 
@@ -27,6 +28,26 @@ const routes: Routes = [
     {
         "component": InstancesComponent,
         "path": "instances/:recipeInstanceId",
+    },
+    {
+        "component": HomeComponent,
+        "path": ":prefix"
+    },
+    {
+        "component": RecipesComponent,
+        "path": ":prefix/recipes"
+    },
+    {
+        "component": InteractionsComponent,
+        "path": ":prefix/interactions",
+    },
+    {
+        "component": InstancesComponent,
+        "path": ":prefix/instances",
+    },
+    {
+        "component": InstancesComponent,
+        "path": ":prefix/instances/:recipeInstanceId",
     },
     {
         "component": NotFoundComponent,
--- http/baker-http-dashboard/src/app/app.settings.ts
+++ http/baker-http-dashboard/src/app/app.settings.ts
@@ -23,43 +23,33 @@ export class AppSettingsService {
     constructor (private http: HttpClient) {
     }
 
-    private getSettingsFile():Promise<String> {
-      return new Promise<String>((resolve, reject) => {
-        this.http.get(LOCAL_SETTINGS_LOCATION).toPromise().then(response => {
-           AppSettingsService.prefix = <PrefixSettings>response;
-           resolve((<PrefixSettings>response).prefix);
-        }).catch((response: any) => {
-           reject(Error(`Could not load file '${SETTINGS_LOCATION}': ${JSON.stringify(response)}`));
-        })
-      })
-    }
-
-    private getAppSettings(prefix: String):Promise<void> {
+    public getAppSettings(prefix: String):Promise<void> {
       return new Promise<void>((resolve, reject) => {
         //For testing purposes:
-//          AppSettingsService.settings = {
-//            "applicationName": "Test",
-//            "apiPath": "/api/bakery",
-//            "clusterInformation": {}
-//          };
-//          resolve()
+         AppSettingsService.settings = {
+           "applicationName": "Test",
+           "apiPath": "/api/bakery",
+           "clusterInformation": {"1": "2"}
+         };
+         resolve()
 
-        this.http.get(prefix + "/" + SETTINGS_LOCATION).toPromise().then(response => {
-           AppSettingsService.settings = <AppSettings>response;
-           resolve();
-        }).catch((response: any) => {
-            reject(Error(`Could not load file '${SETTINGS_LOCATION}': ${JSON.stringify(response)}`))
-        })
+//         this.http.get(prefix + "/" + SETTINGS_LOCATION).toPromise().then(response => {
+//            AppSettingsService.settings = <AppSettings>response;
+//            resolve();
+//         }).catch((response: any) => {
+//             reject(Error(`Could not load file '${SETTINGS_LOCATION}': ${JSON.stringify(response)}`))
+//         })
       })
     }
 
     load () {
+      console.log(window.location.href.substring(window.location.href.lastIndexOf('/') + 1));
+
+      AppSettingsService.prefix = {"prefix": window.location.href.substring(window.location.href.lastIndexOf('/') + 1)}
+      this.getAppSettings(AppSettingsService.prefix.prefix)
       return new Promise<void>((resolve, reject) => {
-        this.getSettingsFile()
-          .then((prefix: String) => {
-            this.getAppSettings(prefix)
+            this.getAppSettings(AppSettingsService.prefix.prefix)
               .then(_ => resolve())
-          })
-        });
+      });
     }
 }
