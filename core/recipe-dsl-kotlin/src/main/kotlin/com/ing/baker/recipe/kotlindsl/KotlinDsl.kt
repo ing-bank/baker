8ef64bccfdbc125a37b00429e6a2eb0ba4d58630
Willem Veelenturf <willem.veelenturf@ing.com>
2023-03-08 21:32:10 +0100

    Implement default failure strategy

--- core/recipe-dsl-kotlin/src/main/kotlin/com/ing/baker/recipe/kotlindsl/KotlinDsl.kt
+++ core/recipe-dsl-kotlin/src/main/kotlin/com/ing/baker/recipe/kotlindsl/KotlinDsl.kt
@@ -7,6 +7,7 @@ import com.ing.baker.recipe.common.EventOutputTransformer
 import com.ing.baker.recipe.common.Ingredient
 import com.ing.baker.recipe.javadsl.Event
 import com.ing.baker.recipe.javadsl.InteractionDescriptor
+import com.ing.baker.recipe.javadsl.InteractionFailureStrategy
 import com.ing.baker.recipe.javadsl.Recipe
 import com.ing.baker.types.Converters
 import com.ing.baker.types.Value
@@ -15,101 +16,142 @@ import kotlin.jvm.internal.CallableReference
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.jvm.javaType
+import kotlin.time.Duration
+import kotlin.time.toJavaDuration
+
+
+@DslMarker
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+annotation class Scoped
 
 fun interactionFunctionToCommonInteraction(builder: InteractionBuilder): InteractionDescriptor {
-   val inputIngredients = builder.func.parameters.drop(1)
-      .map {
-         val type = Converters.readJavaType(it.type.javaType)
-         Ingredient(it.name, type)
-      }
+    val inputIngredients = builder.func.parameters.drop(1)
+        .map {
+            val type = Converters.readJavaType(it.type.javaType)
+            Ingredient(it.name, type)
+        }
 
-   return InteractionDescriptor(
-      builder.func.ownerClass().simpleName,
-      inputIngredients.toScalaSeq(),
-      builder.events.map { Event(it.java, Option.empty()) }.toScalaSeq(),
-      builder.requiredEvents.map { it.java.simpleName }.toScalaSet(),
-      setOf<scala.collection.immutable.Set<String>>().toScalaSet(),
-      mapOf<String, Value>().toScalaMap(),
-      mapOf<String, String>().toScalaMap(),
-      Option.empty(),
-      Option.empty(),
-      Option.empty(),
-      mapOf<com.ing.baker.recipe.common.Event, EventOutputTransformer>().toScalaMap(),
-      Option.empty(),
-   )
+    return InteractionDescriptor(
+        builder.func.ownerClass().simpleName,
+        inputIngredients.toScalaSeq(),
+        builder.events.map { Event(it.java, Option.empty()) }.toScalaSeq(),
+        builder.requiredEvents.map { it.java.simpleName }.toScalaSet(),
+        setOf<scala.collection.immutable.Set<String>>().toScalaSet(),
+        mapOf<String, Value>().toScalaMap(),
+        mapOf<String, String>().toScalaMap(),
+        Option.empty(),
+        Option.empty(),
+        Option.empty(),
+        mapOf<com.ing.baker.recipe.common.Event, EventOutputTransformer>().toScalaMap(),
+        Option.empty(),
+    )
 }
 
 fun convertRecipe(builder: RecipeBuilder): Recipe {
-   return Recipe(builder.name)
-      .withInteractions(builder.interactions.map(::interactionFunctionToCommonInteraction).toScalaSeq())
-      // TODO differentiate between events with and without firing limit.
-      .withSensoryEvents(builder.sensoryEvents.map { it.kClass.java }.toScalaSeq())
+    return Recipe(builder.name)
+        .withInteractions(builder.interactions.map(::interactionFunctionToCommonInteraction).toScalaSeq())
+        // TODO differentiate between events with and without firing limit.
+        .withSensoryEvents(builder.sensoryEvents.map { it.kClass.java }.toScalaSeq())
+        .withDefaultFailureStrategy(InteractionFailureStrategy.RetryWithIncrementalBackoffBuilder()
+            .withInitialDelay(builder.defaultFailureStrategy.initialDelay?.toJavaDuration())
+            .withDeadline(builder.defaultFailureStrategy.deadline?.toJavaDuration())
+            .withMaxTimeBetweenRetries(builder.defaultFailureStrategy.maxTimeBetweenRetries?.toJavaDuration())
+            .build()
+        )
 }
 
 fun KFunction<*>.ownerClass(): KClass<*> {
-   val owner = (this as CallableReference).owner
-   return (owner as KClass<*>)
+    val owner = (this as CallableReference).owner
+    return (owner as KClass<*>)
 }
 
+@Scoped
 class InteractionsBuilder {
 
-   var interactions: Set<InteractionBuilder> = setOf()
+    var interactions: Set<InteractionBuilder> = setOf()
 
-   fun interaction(func: KFunction<*>) {
-      val builder = InteractionBuilder()
-      builder.func(func)
-      interactions =  interactions + builder
-   }
-   fun interaction(func: KFunction<*>, init: InteractionBuilder.() -> Unit) {
-      val builder = InteractionBuilder()
-      builder.apply(init)
-      builder.func(func)
-      interactions =  interactions + builder
-   }
+    fun interaction(func: KFunction<*>) {
+        val builder = InteractionBuilder()
+        builder.func(func)
+        interactions = interactions + builder
+    }
 
-   fun build() = interactions
+    fun interaction(func: KFunction<*>, init: InteractionBuilder.() -> Unit) {
+        val builder = InteractionBuilder()
+        builder.apply(init)
+        builder.func(func)
+        interactions = interactions + builder
+    }
+
+    fun build() = interactions
 }
 
+@Scoped
 class InteractionBuilder {
 
-   lateinit var func: KFunction<*>
-   var events: Set<KClass<*>> = setOf()
-   var requiredEvents: Set<KClass<*>> = setOf()
+    lateinit var func: KFunction<*>
+    var events: Set<KClass<*>> = setOf()
+    var requiredEvents: Set<KClass<*>> = setOf()
 
-   fun func(func: KFunction<*>) {
-      val sealedSubclasses = (func.returnType.classifier as KClass<*>).sealedSubclasses
-      if (sealedSubclasses.isNotEmpty()) {
-         this.events = sealedSubclasses.toSet()
-      } else {
-         this.events = setOf(func.returnType.classifier as KClass<*>)
-      }
+    fun func(func: KFunction<*>) {
+        val sealedSubclasses = (func.returnType.classifier as KClass<*>).sealedSubclasses
+        if (sealedSubclasses.isNotEmpty()) {
+            this.events = sealedSubclasses.toSet()
+        } else {
+            this.events = setOf(func.returnType.classifier as KClass<*>)
+        }
 
-      this.func = func
-   }
+        this.func = func
+    }
 
-   fun events(vararg events: KClass<*>) {
-      this.events = events.toSet()
-   }
+    fun events(vararg events: KClass<*>) {
+        this.events = events.toSet()
+    }
 
-   fun requiredEvents(vararg requiredEvents: KClass<*>) {
-      this.requiredEvents = requiredEvents.toSet()
-   }
+    fun requiredEvents(vararg requiredEvents: KClass<*>) {
+        this.requiredEvents = requiredEvents.toSet()
+    }
 }
 
 fun recipe(name: String, init: RecipeBuilder.() -> Unit): RecipeBuilder {
-   val builder = RecipeBuilder()
-   builder.name = name
-   builder.apply(init)
-   return builder
+    val builder = RecipeBuilder()
+    builder.name = name
+    builder.apply(init)
+    return builder
 }
 
+@Scoped
 class SensoryEventsBuilder {
-   val events = mutableSetOf<KotlinEvent>()
+    val events = mutableSetOf<KotlinEvent>()
 
-   inline fun <reified T> event(maxFiringLimit: Int = 1) = events.add(KotlinEvent(T::class, maxFiringLimit))
-   inline fun <reified T> eventWithoutFiringLimit() = events.add(KotlinEvent(T::class, null))
+    inline fun <reified T> event(maxFiringLimit: Int = 1) = events.add(KotlinEvent(T::class, maxFiringLimit))
+    inline fun <reified T> eventWithoutFiringLimit() = events.add(KotlinEvent(T::class, null))
 
-   fun build() = events.toSet()
+    fun build() = events.toSet()
+}
+
+@Scoped
+class DefaultFailureStrategyBuilder {
+    data class DefaultFailureStrategy(
+        val initialDelay: Duration? = null,
+        val deadline: Duration? = null,
+        val maxTimeBetweenRetries: Duration? = null
+    )
+
+    var defaultFailureStrategy = DefaultFailureStrategy()
+
+    fun initialDelay(initialDelay: Duration) = defaultFailureStrategy
+        .copy(initialDelay = initialDelay)
+        .also { this.defaultFailureStrategy = it }
+
+    fun deadline(deadline: Duration) = defaultFailureStrategy
+        .copy(deadline = deadline)
+        .also { this.defaultFailureStrategy = it }
+
+    fun maxTimeBetweenRetries(maxTimeBetweenRetries: Duration) = defaultFailureStrategy
+        .copy(maxTimeBetweenRetries = maxTimeBetweenRetries)
+        .also { this.defaultFailureStrategy = it }
+    fun build() = defaultFailureStrategy
 }
 
 
@@ -117,19 +159,25 @@ class SensoryEventsBuilder {
 // We can get ingredients from a KClass via .primaryConstructor?.parameters?.map { Pair(it.name, it.type) }
 data class KotlinEvent(val kClass: KClass<*>, val maxFiringLimit: Int?)
 
+@Scoped
 class RecipeBuilder {
 
-   lateinit var name: String
-   lateinit var interactions: Set<InteractionBuilder>
-   lateinit var sensoryEvents: Set<KotlinEvent>
+    lateinit var name: String
+    lateinit var interactions: Set<InteractionBuilder>
+    lateinit var sensoryEvents: Set<KotlinEvent>
+    lateinit var defaultFailureStrategy: DefaultFailureStrategyBuilder.DefaultFailureStrategy
 
-   fun interactions(init: InteractionsBuilder.() -> Unit) {
-      val builder = InteractionsBuilder()
-      builder.apply(init)
-      this.interactions = builder.build()
-   }
+    fun interactions(init: InteractionsBuilder.() -> Unit) {
+        val builder = InteractionsBuilder()
+        builder.apply(init)
+        this.interactions = builder.build()
+    }
 
-   fun sensoryEvents(init: SensoryEventsBuilder.() -> Unit) {
-      sensoryEvents = SensoryEventsBuilder().apply(init).build()
-   }
+    fun sensoryEvents(init: SensoryEventsBuilder.() -> Unit) {
+        sensoryEvents = SensoryEventsBuilder().apply(init).build()
+    }
+
+    fun defaultFailureStrategy(init: DefaultFailureStrategyBuilder.() -> Unit) {
+        defaultFailureStrategy = DefaultFailureStrategyBuilder().apply(init).build()
+    }
 }
--- core/recipe-dsl-kotlin/src/test/kotlin/com/ing/baker/recipe/kotlindsl/KotlinDslTest.kt
+++ core/recipe-dsl-kotlin/src/test/kotlin/com/ing/baker/recipe/kotlindsl/KotlinDslTest.kt
@@ -1,9 +1,15 @@
 package com.ing.baker.recipe.kotlindsl
 
 import com.example.demo.ScalaExtensions.toJavaIterable
+import com.ing.baker.recipe.common.InteractionFailureStrategy.BlockInteraction as BlockInteraction
+import com.ing.baker.recipe.common.InteractionFailureStrategy.FireEventAfterFailure as FireEventAfterFailure
+import com.ing.baker.recipe.common.InteractionFailureStrategy.RetryWithIncrementalBackoff as RetryWithIncrementalBackoff
 import com.ing.baker.recipe.javadsl.Interaction
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class KotlinDslTest {
 
@@ -22,6 +28,11 @@ class KotlinDslTest {
                     requiredEvents(Interactions.MakePayment.PaymentSuccessful::class)
                 }
             }
+            defaultFailureStrategy{
+                initialDelay(1.0.milliseconds)
+                deadline(2.0.hours)
+                maxTimeBetweenRetries(3.0.seconds)
+            }
         }
 
         val recipe = convertRecipe(recipeBuilder)
@@ -30,23 +41,40 @@ class KotlinDslTest {
 
         assertEquals(recipe.name(), "Name")
 
-        assertEquals(recipe.getInteractions().get(0).name(), "MakePayment")
-        assertEquals(recipe.getInteractions().get(1).name(), "ReserveItems")
-        assertEquals(recipe.getInteractions().get(2).name(), "ShipItems")
+        with(recipe.interactions[0]) {
+            assertEquals("MakePayment", name())
+            assertEquals(emptySet<String>(), requiredEvents().toJavaIterable().toSet())
+            assertEquals(listOf("reservedItems", "paymentInformation"), inputIngredients().toJavaIterable().toList().map { it.name() })
+        }
 
-        assertEquals(recipe.getInteractions().get(0).requiredEvents().toJavaIterable().toSet(), emptySet<String>())
-        assertEquals(recipe.getInteractions().get(1).requiredEvents().toJavaIterable().toSet(), emptySet<String>())
-        assertEquals(recipe.getInteractions().get(2).requiredEvents().toJavaIterable().toSet(), setOf("PaymentSuccessful"))
+        with(recipe.interactions[1]) {
+            assertEquals("ReserveItems", name())
+            assertEquals(emptySet<String>(), requiredEvents().toJavaIterable().toSet())
+            assertEquals(listOf("items"), inputIngredients().toJavaIterable().toList().map { it.name() })
+        }
 
-        assertEquals(recipe.toIngredientList(0), listOf("reservedItems", "paymentInformation"))
-        assertEquals(recipe.toIngredientList(1), listOf("items"))
-        assertEquals(recipe.toIngredientList(2), listOf("shippingAddress", "reservedItems"))
+        with(recipe.interactions[2]) {
+            assertEquals("ShipItems", name())
+            assertEquals(setOf("PaymentSuccessful"), requiredEvents().toJavaIterable().toSet())
+            assertEquals(listOf("shippingAddress", "reservedItems"), inputIngredients().toJavaIterable().toList().map { it.name() })
+        }
 
-        with(recipe.getEvents()) {
+        with(recipe.events) {
             assertEquals(size, 3)
-            assertEquals(get(0).name(), "OrderPlaced")
-            assertEquals(get(1).name(), "PaymentInformationReceived")
-            assertEquals(get(2).name(), "ShippingAddressReceived")
+            assertEquals("OrderPlaced", get(0).name())
+            assertEquals("PaymentInformationReceived", get(1).name())
+            assertEquals("ShippingAddressReceived", get(2).name())
+        }
+
+        with(recipe.defaultFailureStrategy()) {
+            when(this){
+                is RetryWithIncrementalBackoff -> {
+                    assertEquals(1L, this.initialDelay().toMillis())
+                    assertEquals(2410, this.maximumRetries())
+                    assertEquals(3000L, this.maxTimeBetweenRetries().get().toMillis())
+                }
+                else -> error("Classname did not match ")
+            }
         }
     }
 
