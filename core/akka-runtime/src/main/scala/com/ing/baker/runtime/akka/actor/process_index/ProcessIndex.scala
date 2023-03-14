e53ffbd9495eaa94b6da30eeb81f7636123ed0f7
XK00LJ <tim.linschoten@ing.nl>
2023-03-14 14:36:36 +0100

    Handled the situation that the initializeCmd was not handled by the actor. Still need to make it blocking.

--- core/akka-runtime/src/main/scala/com/ing/baker/runtime/akka/actor/process_index/ProcessIndex.scala
+++ core/akka-runtime/src/main/scala/com/ing/baker/runtime/akka/actor/process_index/ProcessIndex.scala
@@ -292,7 +292,9 @@ class ProcessIndex(recipeInstanceIdleTimeout: Option[FiniteDuration],
                 val processState = RecipeInstanceState(recipeInstanceId, Map.empty[String, Value], List.empty)
                 val initializeCmd = Initialize(compiledRecipe.initialMarking, processState)
 
-                createProcessActor(recipeInstanceId, compiledRecipe).forward(initializeCmd)
+                //TODO ensure the initilzeCMD is accepted before we add it ot the index
+                createProcessActor(recipeInstanceId, compiledRecipe)
+                  .forward(initializeCmd)
 
                 val actorMetadata = ActorMetadata(recipeId, recipeInstanceId, createdTime, Active)
 
@@ -309,12 +311,24 @@ class ProcessIndex(recipeInstanceIdleTimeout: Option[FiniteDuration],
 
         case _ if index(recipeInstanceId).isDeleted =>
           sender() ! ProcessDeleted(recipeInstanceId)
-        case _ =>
+        case None =>
+          //TODO decide what to do in this situation.
+          log.error("ProcessInstance not found but it is found in the index")
           sender() ! ProcessAlreadyExists(recipeInstanceId)
+        case Some(actorRef: ActorRef) =>
+          //Temporary solution for the situation that the initializeCmd is not send in the original Bake
+          getCompiledRecipe(recipeId) match {
+            case Some(compiledRecipe) =>
+              val processState = RecipeInstanceState(recipeInstanceId, Map.empty[String, Value], List.empty)
+              val initializeCmd = Initialize(compiledRecipe.initialMarking, processState)
+              actorRef ! initializeCmd
+              sender() ! ProcessAlreadyExists(recipeInstanceId)
+            case None =>
+              sender() ! NoRecipeFound(recipeId)
+          }
       }
 
     case command@ProcessEvent(recipeInstanceId, event, correlationId, _, _) =>
-
       run { responseHandler =>
         for {
           instanceAndMeta <- fetchInstance
