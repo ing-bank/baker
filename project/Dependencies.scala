3d91384a33aa92ae59c99068b8bdcd82d0e5c777
Scala Steward <scala_steward@virtuslab.com>
2023-03-18 16:38:06 +0000

    Update slf4j-api, slf4j-nop to 2.0.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -122,7 +122,7 @@ object Dependencies {
 
   val objenisis = "org.objenesis" % "objenesis" % "3.2"
   val jodaTime = "joda-time" % "joda-time" % "2.10.14"
-  val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.36"
+  val slf4jApi = "org.slf4j" % "slf4j-api" % "2.0.7"
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.11"
   val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "6.4"
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.16.0"
