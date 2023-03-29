4d2ddc3e30fd6d48ce0b563d8832e4ea9b5ab2fe
Willem Veelenturf <willem.veelenturf@ing.com>
2023-03-29 13:08:17 +0200

    Add validation for max firing limit in compiler

--- core/recipe-compiler/src/test/kotlin/RecipeCompilerTest.kt
+++ core/recipe-compiler/src/test/kotlin/RecipeCompilerTest.kt
@@ -21,7 +21,7 @@ class RecipeCompilerTest {
     }
 
     @Test
-    fun `should compile dsl to recipe`() {
+    fun `should compile dsl to recipe without interactions`() {
 
         val recipe = recipe("recipe") {
             sensoryEvents {
@@ -31,11 +31,30 @@ class RecipeCompilerTest {
 
         val compiled = RecipeCompiler.compileRecipe(recipe)
 
+        assertEquals(compiled.validationErrors.size, 1)
+        assertEquals(compiled.validationErrors[0], "No interactions found.")
         assertEquals("bb928e13daf86557", compiled.recipeId())
     }
 
     @Test
-    fun `should compile dsl with interaction to recipe`() {
+    fun `should compile with negative firing limit and give validation error`() {
+
+        val recipe = recipe("recipe") {
+            sensoryEvents {
+                event<EventA>(maxFiringLimit = -1)
+            }
+        }
+
+        val compiled = RecipeCompiler.compileRecipe(recipe)
+
+        assertEquals(compiled.validationErrors.size, 2)
+        assertEquals(compiled.validationErrors[0], "No interactions found.")
+        assertEquals(compiled.validationErrors[1], "MaxFiringLimit should be greater than 0")
+        assertEquals("5142418ff252df08", compiled.recipeId())
+    }
+
+    @Test
+    fun `should compile dsl to recipe`() {
 
         val recipe = recipe("recipe") {
             sensoryEvents {
