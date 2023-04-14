7802a1e0ba88929d001a8c40951c9cca20a61e85
Bas de Groot <bas.de.groot@ing.com>
2023-04-14 14:57:32 +0200

    Refactor test

--- core/baker-interface-kotlin/src/test/kotlin/BakerInterfaceTest.kt
+++ core/baker-interface-kotlin/src/test/kotlin/BakerInterfaceTest.kt
@@ -1,39 +1,55 @@
 import com.ing.baker.runtime.common.Baker
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.lang.reflect.Method
 import kotlin.math.pow
 import kotlin.reflect.full.declaredFunctions
 
 class BakerInterfaceTest {
 
     @Test
-    fun `compare scala and kotlin interfaces`() {
-        val bakerInterfaceNames = Baker::class.java.declaredMethods
-            .filterNot { it.isSynthetic }
-            .map { it.name.replace("\$default\$2", "") } // TODO improve this, the 2 should not be hardcoded
-            .sorted()
-            .drop(1) // Drop $init$
+    fun `compare Scala and Kotlin Baker interfaces`() {
+        assertEquals(methodsInScalaInterface(), methodsInKotlinInterface())
+    }
 
-        val kotlinFunctionNames = mutableListOf<String>()
+    private fun methodsInScalaInterface() = Baker::class.java.declaredMethods
+        .filterNot { it.isSynthetic }
+        .map { it.sanitizedName() }
+        .filterNot { it == "\$init\$" }
+        .sorted()
+
+    // Scala functions with default arguments have $default$ in their name.
+    private fun Method.sanitizedName() = if (name.contains("\$default\$")) {
+        name.split("\$default\$").first()
+    } else {
+        name
+    }
+
+    private fun methodsInKotlinInterface(): List<String> {
+        val methodNames = mutableListOf<String>()
 
         com.ing.baker.runtime.kotlindsl.Baker::class
             .declaredFunctions
             .forEach { function ->
-                val optionalParams = function.parameters.count { param -> param.isOptional }
-                if (optionalParams == 0) {
-                    kotlinFunctionNames.add(function.name)
-                } else {
-                    repeat(2.toDouble().pow(optionalParams.toDouble()).toInt()) {
-                        kotlinFunctionNames.add(function.name)
+                val defaultArgumentsCount = function.parameters.count { param -> param.isOptional }
+                if (defaultArgumentsCount != 0) {
+                    // The Kotlin interface uses default arguments instead of overloads. As a result the Kotlin
+                    // interface has fewer methods. We correct that here.
+                    repeat(2.toDouble().pow(defaultArgumentsCount.toDouble()).toInt()) {
+                        methodNames.add(function.name)
                     }
+                } else {
+                    methodNames.add(function.name)
                 }
             }
 
-        kotlinFunctionNames.add("fireEventAndResolveWhenReceived")
-        kotlinFunctionNames.add("fireEventAndResolveWhenCompleted")
-        kotlinFunctionNames.add("fireEventAndResolveOnEvent")
-        kotlinFunctionNames.add("fireEvent")
+        // We removed a couple of nonsensical overloads from the Kotlin interface. We need to add them here so the
+        // comparison does not fail.
+        methodNames.add("fireEventAndResolveWhenReceived")
+        methodNames.add("fireEventAndResolveWhenCompleted")
+        methodNames.add("fireEventAndResolveOnEvent")
+        methodNames.add("fireEvent")
 
-        assertEquals(bakerInterfaceNames, kotlinFunctionNames.sorted())
+        return methodNames.sorted()
     }
 }
