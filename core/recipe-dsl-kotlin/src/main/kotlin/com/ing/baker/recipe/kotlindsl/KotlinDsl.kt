5117dfc0096fbc66bb4f09675fd642ad7ed7ad04
Bas de Groot <bas.de.groot@ing.com>
2023-05-09 13:40:51 +0200

    1488: Fix transformation of Java events

--- core/recipe-dsl-kotlin/src/main/kotlin/com/ing/baker/recipe/kotlindsl/KotlinDsl.kt
+++ core/recipe-dsl-kotlin/src/main/kotlin/com/ing/baker/recipe/kotlindsl/KotlinDsl.kt
@@ -231,7 +231,7 @@ class InteractionBuilder(private val interactionClass: KClass<out com.ing.baker.
             val eventTransformationsInput = eventTransformations.associate {
                 com.ing.baker.recipe.javadsl.Event(it.from.java, Option.empty()) to it.toEventOutputTransformer()
             }
-
+            // test
             Interaction.of(
                 name,
                 InteractionDescriptor.of(interactionClass.java),
