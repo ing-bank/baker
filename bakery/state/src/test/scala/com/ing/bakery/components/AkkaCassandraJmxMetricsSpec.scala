cb5caaa546fca107d4bc8b1b3b39ef21d6b3688f
XK00LJ <tim.linschoten@ing.nl>
2023-05-16 08:07:12 +0200

    Disabled the AkkaCassandraJmxMetricsSpec since it does not work with Java 17.

--- bakery/state/src/test/scala/com/ing/bakery/components/AkkaCassandraJmxMetricsSpec.scala
+++ bakery/state/src/test/scala/com/ing/bakery/components/AkkaCassandraJmxMetricsSpec.scala
@@ -13,7 +13,7 @@ import com.typesafe.scalalogging.LazyLogging
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.common.TextFormat
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper._
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.{BeforeAndAfterAll, Ignore}
 import org.scalatest.concurrent.Eventually
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.time.{Millis, Seconds, Span}
@@ -28,123 +28,129 @@ import scala.collection.convert.ImplicitConversions._
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext}
 
+
+/**
+  * Test is ignored since CassandraUnit is not supported in Java 17.
+  * We can enable this once this is supported again.
+  */
 @nowarn
+@Ignore
 class AkkaCassandraJmxMetricsSpec extends AnyFreeSpec with LazyLogging with Eventually with BeforeAndAfterAll {
 
-//  import TestActors._
-//
-//  implicit override val patienceConfig: PatienceConfig =
-//    PatienceConfig(timeout = scaled(Span(2, Seconds)), interval = scaled(Span(5, Millis)))
-//
-//  implicit val ec: ExecutionContext = ExecutionContext.global
-//  private var system: Option[ActorSystem] = None
-//  private var persistenceInitActor: Option[ActorRef] = None
-//
-//  // Currently apple silicon architecture is not supported (and no workarounds exist). Skip this test in that case.
-//  // more info https://stackoverflow.com/questions/68315912/how-to-start-cassandra-on-a-m1-macbook
-//  // TODO: fix when a fix is available.
-//  val runningOnAppleSilicon: Boolean = System.getProperty("os.arch") == "aarch64"
-//
-//  override protected def beforeAll(): Unit = {
-//    if (!runningOnAppleSilicon) {
-//      try {
-//        startEmbeddedCassandra("cassandra-server.yaml")
-//      } catch {
-//        case _: Exception =>
-//      }
-//      system = Some(ActorSystem("baker"))
-//      persistenceInitActor = system.map(_.actorOf(Props(classOf[AwaitPersistenceInit]), s"persistenceInit-${UUID.randomUUID().toString}"))
-//    }
-//  }
-//
-//  override protected def afterAll(): Unit = {
-//    system.map(_.terminate())
-//  }
-//
-//  def createSession: CqlSession = {
-//    CqlSession.builder()
-//      .withLocalDatacenter("datacenter1")
-//      .addContactPoints(List(InetSocketAddress.createUnresolved(getHost, getNativeTransportPort)).asJavaCollection)
-//      .build()
-//  }
-//
-//  "Launch cassandra and akka app, and ensure it works" - {
-//
-//    "starts actor system and pings the bootstrap actor" in {
-//      if (!runningOnAppleSilicon) {
-//        pingActor
-//      }
-//    }
-//
-//    "after some activity exports some jmx metrics" in {
-//      if (!runningOnAppleSilicon) {
-//        val session = createSession
-//        session.execute("create table akka.test (id int, a text, b text, primary key(id))")
-//        for (i <- 1 to 100) {
-//          session
-//            .execute(
-//              insertInto("akka", "test")
-//                .values(Map[String, Term](
-//                  "id" -> literal(i),
-//                  "a" -> literal(s"A${i}"),
-//                  "b" -> literal(s"B$i")).asJava).build()
-//            )
-//        }
-//
-//        import java.lang.management.ManagementFactory
-//        val mbs = ManagementFactory.getPlatformMBeanServer
-//        mbs.queryMBeans(null, null).map(_.getObjectName).filter(_.toString.startsWith("akka")).foreach(println)
-//        val datastaxMbeans = mbs.queryMBeans(null, null).filter(_.getObjectName.getDomain == "com.datastax.oss.driver")
-//        //      datastaxMbeans.foreach(println)
-//        assert(datastaxMbeans.nonEmpty)
-//      }
-//    }
-//
-//    "ensure prometheus JMX scraping is working" in {
-//      if (!runningOnAppleSilicon) {
-//        for (i <- 1 to 100000) {
-//          pingActor
-//        }
-//
-//        val prometheusRegistry = MetricService.defaultInstance.registry
-//
-//        val writer = new CharArrayWriter(16 * 1024)
-//        TextFormat.write004(writer, prometheusRegistry.metricFamilySamples)
-//        val content = writer.toString
-//
-//        assert(content.split("\n").exists(_.startsWith("cassandra_cql")))
-//      }
-//    }
-//  }
-//
-//
-//  private def pingActor = {
-//    val r = Await.result(persistenceInitActor.get.ask(Ping)(Timeout.durationToTimeout(30 seconds)), 40 seconds)
-//    assert(r.toString == "Pong")
-//  }
-//}
-//
-//object TestActors {
-//
-//  case object Ping extends NoSerializationVerificationNeeded
-//
-//  case object Pong extends NoSerializationVerificationNeeded
-//
-//  class AwaitPersistenceInit extends PersistentActor with ActorLogging with PersistentActorMetrics {
-//
-//    override val persistenceId: String = s"persistenceInit-${UUID.randomUUID()}"
-//
-//    log.info("Starting PersistenceInit actor with id: {}", persistenceId)
-//
-//    // intentionally left empty
-//    def receiveRecover: Receive = Map.empty
-//
-//    // intentionally left empty
-//    def receiveCommand: Receive = {
-//      case Ping =>
-//        sender() ! Pong
-//    }
-//  }
+  import TestActors._
+
+  implicit override val patienceConfig: PatienceConfig =
+    PatienceConfig(timeout = scaled(Span(2, Seconds)), interval = scaled(Span(5, Millis)))
+
+  implicit val ec: ExecutionContext = ExecutionContext.global
+  private var system: Option[ActorSystem] = None
+  private var persistenceInitActor: Option[ActorRef] = None
+
+  // Currently apple silicon architecture is not supported (and no workarounds exist). Skip this test in that case.
+  // more info https://stackoverflow.com/questions/68315912/how-to-start-cassandra-on-a-m1-macbook
+  // TODO: fix when a fix is available.
+  val runningOnAppleSilicon: Boolean = System.getProperty("os.arch") == "aarch64"
+
+  override protected def beforeAll(): Unit = {
+    if (!runningOnAppleSilicon) {
+      try {
+        startEmbeddedCassandra("cassandra-server.yaml")
+      } catch {
+        case _: Exception =>
+      }
+      system = Some(ActorSystem("baker"))
+      persistenceInitActor = system.map(_.actorOf(Props(classOf[AwaitPersistenceInit]), s"persistenceInit-${UUID.randomUUID().toString}"))
+    }
+  }
+
+  override protected def afterAll(): Unit = {
+    system.map(_.terminate())
+  }
+
+  def createSession: CqlSession = {
+    CqlSession.builder()
+      .withLocalDatacenter("datacenter1")
+      .addContactPoints(List(InetSocketAddress.createUnresolved(getHost, getNativeTransportPort)).asJavaCollection)
+      .build()
+  }
+
+  "Launch cassandra and akka app, and ensure it works" - {
+
+    "starts actor system and pings the bootstrap actor" in {
+      if (!runningOnAppleSilicon) {
+        pingActor
+      }
+    }
+
+    "after some activity exports some jmx metrics" in {
+      if (!runningOnAppleSilicon) {
+        val session = createSession
+        session.execute("create table akka.test (id int, a text, b text, primary key(id))")
+        for (i <- 1 to 100) {
+          session
+            .execute(
+              insertInto("akka", "test")
+                .values(Map[String, Term](
+                  "id" -> literal(i),
+                  "a" -> literal(s"A${i}"),
+                  "b" -> literal(s"B$i")).asJava).build()
+            )
+        }
+
+        import java.lang.management.ManagementFactory
+        val mbs = ManagementFactory.getPlatformMBeanServer
+        mbs.queryMBeans(null, null).map(_.getObjectName).filter(_.toString.startsWith("akka")).foreach(println)
+        val datastaxMbeans = mbs.queryMBeans(null, null).filter(_.getObjectName.getDomain == "com.datastax.oss.driver")
+        //      datastaxMbeans.foreach(println)
+        assert(datastaxMbeans.nonEmpty)
+      }
+    }
+
+    "ensure prometheus JMX scraping is working" in {
+      if (!runningOnAppleSilicon) {
+        for (i <- 1 to 100000) {
+          pingActor
+        }
+
+        val prometheusRegistry = MetricService.defaultInstance.registry
+
+        val writer = new CharArrayWriter(16 * 1024)
+        TextFormat.write004(writer, prometheusRegistry.metricFamilySamples)
+        val content = writer.toString
+
+        assert(content.split("\n").exists(_.startsWith("cassandra_cql")))
+      }
+    }
+  }
+
+
+  private def pingActor = {
+    val r = Await.result(persistenceInitActor.get.ask(Ping)(Timeout.durationToTimeout(30 seconds)), 40 seconds)
+    assert(r.toString == "Pong")
+  }
+}
+
+object TestActors {
+
+  case object Ping extends NoSerializationVerificationNeeded
+
+  case object Pong extends NoSerializationVerificationNeeded
+
+  class AwaitPersistenceInit extends PersistentActor with ActorLogging with PersistentActorMetrics {
+
+    override val persistenceId: String = s"persistenceInit-${UUID.randomUUID()}"
+
+    log.info("Starting PersistenceInit actor with id: {}", persistenceId)
+
+    // intentionally left empty
+    def receiveRecover: Receive = Map.empty
+
+    // intentionally left empty
+    def receiveCommand: Receive = {
+      case Ping =>
+        sender() ! Pong
+    }
+  }
 
 
 }
