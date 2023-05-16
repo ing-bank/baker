893d2f942d8b1fd693c0e8fb265439c065adc85a
Bas de Groot <bas.de.groot@ing.com>
2023-05-16 14:37:02 +0200

    More experimenting with reflection

---
+++ core/baker-interface-kotlin/src/main/kotlin/com/ing/baker/runtime/kotlindsl/KBaker.kt
@@ -0,0 +1,16 @@
+package com.ing.baker.runtime.kotlindsl
+
+import com.ing.baker.runtime.javadsl.Baker
+import kotlin.reflect.KVisibility
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.jvm.isAccessible
+
+fun main() {
+    val bakerClass = Baker::class
+
+    bakerClass.declaredFunctions
+        .filter { it.visibility == KVisibility.PUBLIC }
+        .map { member -> Pair(member.name, member.returnType.arguments.firstOrNull()?.type ?: member.returnType) }
+        .forEach { println("${it.first} - ${it.second}") }
+}
