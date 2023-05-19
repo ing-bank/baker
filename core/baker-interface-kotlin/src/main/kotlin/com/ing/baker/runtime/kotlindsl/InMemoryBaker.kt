26e4c10829bd1dfea421445c0d5a509a0b63a034
Bas de Groot <bas.de.groot@ing.com>
2023-05-19 10:49:14 +0200

    Use default argument

--- core/baker-interface-kotlin/src/main/kotlin/com/ing/baker/runtime/kotlindsl/InMemoryBaker.kt
+++ core/baker-interface-kotlin/src/main/kotlin/com/ing/baker/runtime/kotlindsl/InMemoryBaker.kt
@@ -4,9 +4,7 @@ import com.ing.baker.runtime.inmemory.InMemoryBaker
 import com.ing.baker.runtime.model.BakerF
 
 object InMemoryBaker {
-    fun kotlin() = Baker(InMemoryBaker.java())
-
-    fun kotlin(implementations: List<Any>) = Baker(InMemoryBaker.java(implementations))
+    fun kotlin(implementations: List<Any> = emptyList()) = Baker(InMemoryBaker.java(implementations))
 
     fun kotlin(config: BakerF.Config, implementations: List<Any>) = Baker(InMemoryBaker.java(config, implementations))
 }
