005f3674ea7f3c2d619c4c4221df05ddeb20d5bb
Bas de Groot <bas.de.groot@ing.com>
2023-05-19 10:43:04 +0200

    Remove interface and hide constructor

--- core/baker-interface-kotlin/src/test/kotlin/BakerInterfaceTest.kt
+++ core/baker-interface-kotlin/src/test/kotlin/BakerInterfaceTest.kt
@@ -4,34 +4,34 @@ import java.lang.reflect.Method
 import kotlin.math.pow
 import kotlin.reflect.full.declaredFunctions
 
-class KotlinBakerInterfaceTest {
+class BakerInterfaceTest {
 
     @Test
-    fun `compare Scala and Kotlin Baker interfaces`() {
+    fun `compare methods in Scala trait and Kotlin Baker class`() {
         @Suppress("ConvertArgumentToSet") // Some method names occur multiple times due to overloads
-        val missingInKotlinInterface = methodsInScalaInterface().minus(methodsInKotlinInterface())
+        val missingInKotlinInterface = methodsInScalaTrait().minus(methodsInKotlinClass())
 
         if (missingInKotlinInterface.isNotEmpty()) {
             fail("Failed to find $missingInKotlinInterface in the Kotlin interface")
         }
     }
 
-    private fun methodsInScalaInterface() = com.ing.baker.runtime.common.Baker::class.java.declaredMethods
+    private fun methodsInScalaTrait() = com.ing.baker.runtime.common.Baker::class.java.declaredMethods
         .filterNot { it.isSynthetic }
         .map { it.sanitizedName() }
         .filterNot { it == "\$init\$" }
 
-    // Scala functions with default arguments have $default$ in their name.
+    // Scala methods with default arguments have $default$ in their name.
     private fun Method.sanitizedName() = if (name.contains("\$default\$")) {
         name.split("\$default\$").first()
     } else {
         name
     }
 
-    private fun methodsInKotlinInterface(): List<String> {
+    private fun methodsInKotlinClass(): List<String> {
         val methodNames = com.ing.baker.runtime.kotlindsl.Baker::class.declaredFunctions
             .fold(initial = emptyList<String>()) { names, kFunction ->
-                // The Kotlin interface uses default arguments instead of overloads. As a result the Kotlin
+                // The Kotlin implementation uses default arguments instead of overloads. As a result the Kotlin
                 // interface has fewer methods. We correct that here.
                 val defaultArgumentsCount = kFunction.parameters.count { param -> param.isOptional }
                 if (defaultArgumentsCount != 0) {
