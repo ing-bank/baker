98e6885a70bc4bc1fcd3d0b91de584574af2f12c
XK00LJ <tim.linschoten@ing.nl>
2023-05-24 16:16:51 +0200

    Added missing function to Kotlin interface.

--- core/baker-interface-kotlin/src/main/kotlin/com/ing/baker/runtime/kotlindsl/Baker.kt
+++ core/baker-interface-kotlin/src/main/kotlin/com/ing/baker/runtime/kotlindsl/Baker.kt
@@ -113,6 +113,9 @@ class Baker internal constructor(private val jBaker: Baker) : AutoCloseable {
     suspend fun getIngredients(recipeInstanceId: String): Map<String, Value> =
         jBaker.getIngredients(recipeInstanceId).await()
 
+    suspend fun getIngredient(recipeInstanceId: String, name: String): Value =
+            jBaker.getIngredient(recipeInstanceId, name).await()
+
     suspend fun getEvents(recipeInstanceId: String): List<EventMoment> =
         jBaker.getEvents(recipeInstanceId).await()
 
