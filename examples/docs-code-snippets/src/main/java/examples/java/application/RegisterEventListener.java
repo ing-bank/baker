63a057c7a0c68aaba3efd362a59564c50c7cfa5a
Bas de Groot <bas.de.groot@ing.com>
2023-06-06 16:05:00 +0200

    Fix example

--- examples/docs-code-snippets/src/main/java/examples/java/application/RegisterEventListener.java
+++ examples/docs-code-snippets/src/main/java/examples/java/application/RegisterEventListener.java
@@ -3,8 +3,6 @@ package examples.java.application;
 import com.ing.baker.runtime.javadsl.Baker;
 import com.ing.baker.runtime.javadsl.EventInstance;
 
-import java.util.function.BiConsumer;
-
 public class RegisterEventListener {
 
     private final Baker baker;
@@ -14,9 +12,8 @@ public class RegisterEventListener {
     }
 
     public void example() {
-        BiConsumer<String, EventInstance> handler = (String recipeInstanceId, EventInstance event) ->
-                System.out.println("Recipe Instance " + recipeInstanceId + " processed event " + event.name());
-
-        baker.registerEventListener(handler);
+        baker.registerEventListener((String recipeInstanceId, EventInstance event) ->
+            System.out.println("Recipe Instance " + recipeInstanceId + " processed event " + event.name())
+        );
     }
 }
