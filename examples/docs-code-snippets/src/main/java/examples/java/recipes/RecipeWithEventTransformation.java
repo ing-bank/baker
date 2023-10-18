5c73971bd4b2ac7066f7545d169d1f3943a6c6a7
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2023-10-18 08:23:56 +0200

    Fixed output ingredient rename example/doc for Java

--- examples/docs-code-snippets/src/main/java/examples/java/recipes/RecipeWithEventTransformation.java
+++ examples/docs-code-snippets/src/main/java/examples/java/recipes/RecipeWithEventTransformation.java
@@ -10,8 +10,9 @@ public class RecipeWithEventTransformation {
     public final static Recipe recipe = new Recipe("example")
         .withInteractions(
             InteractionDescriptor.of(ShipOrder.class)
-                .withEventTransformation(OrderPlaced.class, "OrderCreated")
-                .renameRequiredIngredient("customerId", "userId")
-                .renameRequiredIngredient("productIds", "skus")
+                .withEventTransformation(OrderPlaced.class, "OrderCreated",
+                        Map.of("customerId", "userId",
+                               "productIds", "skus")
+                )
         );
 }
