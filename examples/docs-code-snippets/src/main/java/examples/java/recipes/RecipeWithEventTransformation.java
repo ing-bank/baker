c5f9d6b80042cecd61ce1abb35a4e9e5e6f780f9
Elmar Wachtmeester <elmar@wachtmeestertjes.nl>
2023-10-30 13:18:40 +0100

    Fixed output ingredient rename example/doc for Java (#1574)
    * Fixed output ingredient rename example/doc for Java
    
    * fixed forgotten import

--- examples/docs-code-snippets/src/main/java/examples/java/recipes/RecipeWithEventTransformation.java
+++ examples/docs-code-snippets/src/main/java/examples/java/recipes/RecipeWithEventTransformation.java
@@ -5,13 +5,16 @@ import com.ing.baker.recipe.javadsl.Recipe;
 import examples.java.events.OrderPlaced;
 import examples.java.interactions.ShipOrder;
 
+import java.util.Map;
+
 public class RecipeWithEventTransformation {
 
     public final static Recipe recipe = new Recipe("example")
         .withInteractions(
             InteractionDescriptor.of(ShipOrder.class)
-                .withEventTransformation(OrderPlaced.class, "OrderCreated")
-                .renameRequiredIngredient("customerId", "userId")
-                .renameRequiredIngredient("productIds", "skus")
+                .withEventTransformation(OrderPlaced.class, "OrderCreated",
+                        Map.of("customerId", "userId",
+                               "productIds", "skus")
+                )
         );
 }
