262c521c7c03ecd39752db6d9cbf2fd300ccad06
Scala Steward <scala_steward@virtuslab.com>
2024-05-07 20:06:21 +0000

    Update http4s-circe, http4s-core, ... to 0.23.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val akkaManagementVersion = "1.1.4"
   val akkaPersistenceCassandraVersion = "1.0.6"
   val akkaHttpVersion = "10.2.9"
-  val http4sVersion = "0.21.34"
+  val http4sVersion = "0.23.27"
   val fs2Version = "2.5.10"
   val circeVersion = "0.14.2"
   val mockitoScalaVersion = "1.17.14"
