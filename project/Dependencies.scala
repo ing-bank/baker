313277e8989e9e37c6f5a786f9a6cea1b2557325
Scala Steward <scala_steward@virtuslab.com>
2024-08-27 15:30:22 +0000

    Update logback-classic to 1.5.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -124,7 +124,7 @@ object Dependencies {
   val objenisis = "org.objenesis" % "objenesis" % "3.2"
   val jodaTime = "joda-time" % "joda-time" % "2.12.7"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "2.0.16"
-  val logback = "ch.qos.logback" % "logback-classic" % "1.4.14"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.5.7"
   val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "7.3"
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.17.1"
   val scalaCheckPlus = "org.scalatestplus" %% "scalacheck-1-17" % "3.2.18.0"
