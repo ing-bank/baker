4013bbd516ee136caa4dede89e2bb709329418ea
Bekir Oguz <bekiroguz@gmail.com>
2024-09-19 18:43:34 +0200

    Configure automated ci-release pipeline (#1758)
    * Create sonatype release pipeline file (#5)
    
    * Create sonatype release pipeline file
    
    * fix repo name check condition
    
    * configure branch name input
    
    * Update branch name input description
    
    * use standard github actions GITHUB_REF for the source branch name extraction
    
    * remove github username and token
    
    * commented out PGP_PASSPHRASE and PGP_SECRET variables in publish.yml
    
    * Refactor publish step in Publish.scala to use "+publish" instead of "+publishSigned"
    
    * use proper sbt release command using quotes
    
    * Refactor release workflow and publish step using sbt-ci-release plugin
    
    * override release command to publish to local only
    
    * remove overridden release commands
    
    * Refactor versioning script and remove unused version.sbt file
    
    * Refactor release workflow to include pull request trigger
    
    * Refactor release workflow to remove unused publish.yml file
    
    * merge all pipelines into one file for better overview and ability to depend on specific tasks
    
    * Add custom labels to PRs in .scala-steward.conf

--- project/Publish.scala
+++ project/Publish.scala
@@ -1,14 +1,12 @@
 import sbt._
 import Keys._
-import sbtrelease.ReleasePlugin.autoImport._
-import sbtrelease.ReleaseStateTransformations._
 import xerial.sbt.Sonatype.SonatypeKeys._
 
 object Publish {
 
   lazy val settings =
     if (sys.env.contains("FEEDURL")) StableToAzureFeed
-    else if ( (sys.env.contains("USERNAME"))) ReleaseToSonatype
+    else if ( (sys.env.contains("SONATYPE_USERNAME"))) ReleaseToSonatype
     else SuppressJavaDocs
 
   import aether.AetherKeys._
@@ -26,63 +24,17 @@ object Publish {
     aetherDeploy / logLevel  := Level.Info
   )
 
-  protected def isSnapshot(s: String) = s.trim endsWith "SNAPSHOT"
-
-  protected val nexus = "https://oss.sonatype.org/"
-  protected val ossSnapshots = "Sonatype OSS Snapshots" at nexus + "content/repositories/snapshots/"
-  protected val ossStaging = "Sonatype OSS Staging" at nexus + "service/local/staging/deploy/maven2/"
-
-  val ReleaseToSonatype = Seq(
-    credentials ++= Seq(
-      Credentials(
-      "Sonatype Nexus Repository Manager",
-      "oss.sonatype.org",
-      sys.env.getOrElse("USERNAME", ""),
-      sys.env.getOrElse("PASSWORD", "")
-    ),
-      Credentials(
-        "GnuPG Key ID",
-        "gpg",
-        "303489A85EBB77F6E93E2A254CCF1479F92AE2B7", // key identifier
-        "ignored" // this field is ignored; passwords are supplied by pinentry
-      )
-    ),
-    releaseIgnoreUntrackedFiles := true,
-    sonatypeProfileName := "com.ing",
-    licenses := Seq("MIT" -> url("https://opensource.org/licenses/MIT")),
+  val ReleaseToSonatype = inThisBuild(List(
+    organization := "com.ing",
     homepage := Some(url("https://github.com/ing-bank/baker")),
-    scmInfo := Some(ScmInfo(
-      browseUrl = url("https://github.com/ing-bank/baker"),
-      connection = "scm:git@github.com:ing-bank/baker.git")),
-    pomExtra := (
-      <developers>
-        <developer>
-          <id>Apollo</id>
-          <name>Squad Apollo</name>
-        </developer>
-      </developers>
-    ),
-    publishMavenStyle := true,
-    publishTo := version((v: String) => Some(if (isSnapshot(v)) ossSnapshots else ossStaging)).value,
-    Test / publishArtifact := false,
-    packageDoc / publishArtifact := true,
-    packageSrc / publishArtifact := true,
-    pomIncludeRepository := (_ => false),
-    releaseCrossBuild := true,
-    releaseProcess := Seq[ReleaseStep](
-      releaseStepCommand("sonatypeDropAll"),
-      checkSnapshotDependencies,
-      inquireVersions,
-      runClean,
-      runTest,
-      setReleaseVersion,
-      commitReleaseVersion,
-      tagRelease,
-      releaseStepCommandAndRemaining("+publish"),
-      setNextVersion,
-      commitNextVersion,
-      releaseStepCommand("sonatypeReleaseAll"),
-      pushChanges
+    licenses := List(License.MIT),
+    developers := List(
+      Developer(
+        "Apollo",
+        "Squad Apollo",
+        "ML-DBZ-Service-Apollo@ing.nl",
+        url("https://ing-bank.github.io/baker/")
+      )
     )
-  )
+  ))
 }
