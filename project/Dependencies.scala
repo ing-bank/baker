279371677cc0370a94d11983a22a180470188fb9
Scala Steward <scala_steward@virtuslab.com>
2024-12-21 16:52:46 +0000

    Update kotlinx-coroutines-core, ... to 1.10.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -150,8 +150,8 @@ object Dependencies {
 
   val guava = "com.google.guava" % "guava" % "33.3.1-jre"
 
-  val kotlinXCoroutinesCore = "org.jetbrains.kotlinx" % "kotlinx-coroutines-core" % "1.7.3" pomOnly()
-  val kotlinXCoroutinesJdk8 = "org.jetbrains.kotlinx" % "kotlinx-coroutines-jdk8" % "1.7.3"
+  val kotlinXCoroutinesCore = "org.jetbrains.kotlinx" % "kotlinx-coroutines-core" % "1.10.1" pomOnly()
+  val kotlinXCoroutinesJdk8 = "org.jetbrains.kotlinx" % "kotlinx-coroutines-jdk8" % "1.10.1"
 
   def scopeDeps(scope: String, modules: Seq[ModuleID]) = modules.map(m => m % scope)
 
