4155717cd9061ed565f037788355109a79b2a10c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-12-26 15:49:16 +0100

    Patch updates (#1814)
    * Update logback-classic to 1.5.14
    
    * Update sbt-ci-release to 1.9.2
    
    * Update junit-jupiter-engine to 5.11.4
    
    * Update spring-context to 6.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val catsEffectVersion = "2.5.5"
   val catsCoreVersion = "2.12.0"
   val scalapbVersion = scalapb.compiler.Version.scalapbVersion
-  val springVersion = "6.2.0"
+  val springVersion = "6.2.1"
   val springBootVersion = "2.6.1"
 
   val akkaInmemoryJournal = ("com.github.dnvriend" %% "akka-persistence-inmemory" % "2.5.15.2")
@@ -31,7 +31,7 @@ object Dependencies {
   val mockitoScalaTest = "org.mockito" %% "mockito-scala-scalatest" % mockitoScalaVersion
   val mockServer = "org.mock-server" % "mockserver-netty" % "5.15.0"
   val junitInterface = "com.github.sbt" % "junit-interface" % "0.13.3"
-  val junitJupiter = "org.junit.jupiter" % "junit-jupiter-engine" % "5.11.3"
+  val junitJupiter = "org.junit.jupiter" % "junit-jupiter-engine" % "5.11.4"
 
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
 
@@ -124,7 +124,7 @@ object Dependencies {
   val objenisis = "org.objenesis" % "objenesis" % "3.4"
   val jodaTime = "joda-time" % "joda-time" % "2.13.0"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "2.0.16"
-  val logback = "ch.qos.logback" % "logback-classic" % "1.5.12"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.5.14"
   val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "7.3"
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.18.1"
   val scalaCheckPlus = "org.scalatestplus" %% "scalacheck-1-17" % "3.2.18.0"
