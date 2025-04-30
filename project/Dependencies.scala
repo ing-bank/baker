1376b1e7503cf9313790878b985fbd7c02ac9787
Scala Steward <scala_steward@virtuslab.com>
2025-04-30 16:15:42 +0000

    Update jackson-core, jackson-databind to 2.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -138,8 +138,8 @@ object Dependencies {
 
   val snakeYaml = "org.yaml" % "snakeyaml" % "2.4"
 
-  val jacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % "2.18.3"
-  val jacksonCore = "com.fasterxml.jackson.core" % "jackson-core" % "2.18.3"
+  val jacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % "2.19.0"
+  val jacksonCore = "com.fasterxml.jackson.core" % "jackson-core" % "2.19.0"
   val jawnParser = "org.typelevel" %% "jawn-parser" % "1.6.0"
   val nettyHandler = "io.netty" % "netty-handler" % "4.1.119.Final"
 
