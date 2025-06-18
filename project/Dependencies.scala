9d34de151b0b68988c9926f5106daeba9f1c6a37
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-06-18 22:33:24 +0200

    Patch updates (#1890)
    * Update jackson-core to 2.19.1
    
    * Update jackson-databind to 2.19.1
    
    * Update spring-context to 6.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val catsEffectVersion = "2.5.5"
   val catsCoreVersion = "2.13.0"
   val scalapbVersion = scalapb.compiler.Version.scalapbVersion
-  val springVersion = "6.2.7"
+  val springVersion = "6.2.8"
   val springBootVersion = "2.6.1"
 
   val akkaInmemoryJournal = ("com.github.dnvriend" %% "akka-persistence-inmemory" % "2.5.15.2")
@@ -138,8 +138,8 @@ object Dependencies {
 
   val snakeYaml = "org.yaml" % "snakeyaml" % "2.4"
 
-  val jacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % "2.19.0"
-  val jacksonCore = "com.fasterxml.jackson.core" % "jackson-core" % "2.19.0"
+  val jacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % "2.19.1"
+  val jacksonCore = "com.fasterxml.jackson.core" % "jackson-core" % "2.19.1"
   val jawnParser = "org.typelevel" %% "jawn-parser" % "1.6.0"
   val nettyHandler = "io.netty" % "netty-handler" % "4.2.2.Final"
 
