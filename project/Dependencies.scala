59ebd3eca3d3e6cd393a4cc4b9cf54fa33d6102c
Scala Steward <scala_steward@virtuslab.com>
2025-09-09 15:28:24 +0000

    Update netty-handler to 4.2.6.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -136,7 +136,7 @@ object Dependencies {
   val jacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % "2.19.1"
   val jacksonCore = "com.fasterxml.jackson.core" % "jackson-core" % "2.19.1"
   val jawnParser = "org.typelevel" %% "jawn-parser" % "1.6.0"
-  val nettyHandler = "io.netty" % "netty-handler" % "4.2.2.Final"
+  val nettyHandler = "io.netty" % "netty-handler" % "4.2.6.Final"
 
   private val bouncycastleVersion = "1.81"
 
