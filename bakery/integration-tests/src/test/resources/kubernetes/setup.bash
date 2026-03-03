6b7e7404061a5dc66bdcd993ad0f3855baae5554
Dragoslav Pavkovic <dragoslav.pavkovic@ing.com>
2026-03-03 20:19:36 +0100

    renaming directories to follow module names

--- bakery/integration-tests/src/test/resources/kubernetes/setup.bash
+++
@@ -1,5 +0,0 @@
-kubectl apply -f cassandra.yaml
-kubectl apply -f kafka-event-sink.yaml
-kubectl apply -f external-interactions.yaml
-kubectl apply -f webshop-baker.yaml
-kubectl apply -f example-client-app.yaml
