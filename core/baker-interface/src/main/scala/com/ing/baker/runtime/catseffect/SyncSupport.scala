9530b314005d609c1240d236be361dda9f8d0c27
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2026-08-25 16:52:54 +0200

    SyncSupport using CompletableFuture

--- core/baker-interface/src/main/scala/com/ing/baker/runtime/catseffect/SyncSupport.scala
+++ core/baker-interface/src/main/scala/com/ing/baker/runtime/catseffect/SyncSupport.scala
@@ -2,6 +2,10 @@ package com.ing.baker.runtime.catseffect
 
 import cats.effect.Sync
 
+import java.util.concurrent.{CompletableFuture, CompletionStage, Executor, Executors}
+import java.util.function.{Function, Supplier}
+import scala.util.control.NonFatal
+
 /**
   * Minimal synchronous effect capabilities used by model components.
   */
@@ -16,6 +20,72 @@ trait SyncSupport[F[_]] {
 }
 
 object SyncSupport {
+  type CompletionStageF[A] = CompletionStage[A]
+  type CompletableFutureF[A] = CompletableFuture[A]
+
+  // Dedicated pool for possibly blocking user interaction code.
+  private val blockingExecutor: Executor =
+    Executors.newCachedThreadPool((r: Runnable) => {
+      val thread = new Thread(r)
+      thread.setName("baker-sync-support-blocking")
+      thread.setDaemon(true)
+      thread
+    })
+
+  private def failedFuture[A](throwable: Throwable): CompletableFuture[A] = {
+    val future = new CompletableFuture[A]()
+    future.completeExceptionally(throwable)
+    future
+  }
+
+  implicit val fromCompletionStage: SyncSupport[CompletionStageF] =
+    new SyncSupport[CompletionStageF] {
+      override def pure[A](value: A): CompletionStage[A] = CompletableFuture.completedFuture(value)
+      override def map[A, B](fa: CompletionStage[A])(f: A => B): CompletionStage[B] =
+        fa.thenApply(new Function[A, B] {
+          override def apply(value: A): B = f(value)
+        })
+      override def flatMap[A, B](fa: CompletionStage[A])(f: A => CompletionStage[B]): CompletionStage[B] =
+        fa.thenCompose(new Function[A, CompletionStage[B]] {
+          override def apply(value: A): CompletionStage[B] = f(value)
+        })
+      override def raiseError[A](throwable: Throwable): CompletionStage[A] = failedFuture(throwable)
+      override def delay[A](thunk: => A): CompletionStage[A] =
+        try pure(thunk)
+        catch {
+          case NonFatal(exception) => raiseError(exception)
+        }
+      override def blocking[A](thunk: => A): CompletionStage[A] =
+        CompletableFuture.supplyAsync(new Supplier[A] {
+          override def get(): A = thunk
+        }, blockingExecutor)
+      override def unit: CompletionStage[Unit] = pure(())
+    }
+
+  implicit val fromCompletableFuture: SyncSupport[CompletableFutureF] =
+    new SyncSupport[CompletableFutureF] {
+      override def pure[A](value: A): CompletableFuture[A] = CompletableFuture.completedFuture(value)
+      override def map[A, B](fa: CompletableFuture[A])(f: A => B): CompletableFuture[B] =
+        fa.thenApply(new Function[A, B] {
+          override def apply(value: A): B = f(value)
+        })
+      override def flatMap[A, B](fa: CompletableFuture[A])(f: A => CompletableFuture[B]): CompletableFuture[B] =
+        fa.thenCompose(new Function[A, CompletableFuture[B]] {
+          override def apply(value: A): CompletableFuture[B] = f(value)
+        })
+      override def raiseError[A](throwable: Throwable): CompletableFuture[A] = failedFuture(throwable)
+      override def delay[A](thunk: => A): CompletableFuture[A] =
+        try pure(thunk)
+        catch {
+          case NonFatal(exception) => raiseError(exception)
+        }
+      override def blocking[A](thunk: => A): CompletableFuture[A] =
+        CompletableFuture.supplyAsync(new Supplier[A] {
+          override def get(): A = thunk
+        }, blockingExecutor)
+      override def unit: CompletableFuture[Unit] = pure(())
+    }
+
   object syntax {
     implicit final class SyncEffectOps[F[_], A](private val fa: F[A]) {
       def map[B](f: A => B)(implicit sync: SyncSupport[F]): F[B] =
